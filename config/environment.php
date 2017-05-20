45e76f61f223471bdc9795588db6fbf4cabab957
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-05-20 13:09:42 -0400

    Added acf pro

--- config/environment.php
+++ config/environment.php
@@ -7,12 +7,12 @@ return function() {
     // Check for the environment variable
 
 
-    if ('local' === $_SERVER['APP_ENV'])
+    if ('production' === $_SERVER['APP_ENV'])
     {
         // Return the environment file slug name: .env.{$slug}.php
-        return 'local';
+        return 'production';
     }
 
     // Else if no environment variable found... it might be a production environment...
-    return 'production';
+    return 'local';
 };
