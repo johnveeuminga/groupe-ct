f27fb956b576c8dce88da74c070c8216d52fa115
Rémi Clermont <remi.clermont@ctrlweb.ca>
2017-05-31 09:58:34 -0400

    b11 b12 same

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/elements/bloc11.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/elements/bloc11.blade.php
@@ -4,57 +4,60 @@
 
 
 
+<div class="default-padding">
+    <div class="col-xs-12">
+
+        <!-- prod-logiciel MAIN CONTAINER START -->
+        <div class="prod-logiciel-main-container default-padding">
+
+            <!-- THE ELEMENT START  -->
+            <div class="prod-logiciel-container"><!-- reverse-elem : to swap img and text div -->
+                <div class="prod-logiciel-img-container">
+                    <div class="prod-logiciel-img"></div>
+                    <span class="prod-logiciel-lang">Vidéo disponible en anglais seulement</span>
+                </div>
+                <div class="buffer"></div>
+                <div class="prod-logiciel-text-container">
+                    <h3 class="prod-logiciel-title">IRISPowerscan</h3>
+                    <p class="prod-logiciel-subtitle">NUMÉRISATION ET TRAITEMENT DE DONNÉES</p>
+                    <p class="prod-logiciel-text">
+                        Nunc elementum venenatis dui, quis tempor nulla molestie quis. Quisque dignissim sagittis
+                        lorem id blandit. Nulla facilisi. Sed blandit quam ut arcu facilisis aliquet. Orci varius
+                        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam aliquam,
+                        turpis non bibendum vestibulum, odio turpis viverra massa
+                    </p>
+                    <a class="brochure-download-pdf-white" href="#"><?php _e('BROCHURE', THEME_TEXTDOMAIN); ?></a>
+                </div>
+            </div>
+            <!-- THE ELEMENT END  -->
+
+        {{--</div><!-- prod-logiciel MAIN CONTAINER END -->--}}
 
 
-<!-- prod-logiciel MAIN CONTAINER START -->
-<div class="prod-logiciel-main-container default-padding">
+        <!-- prod-logiciel MAIN CONTAINER START -->
+        {{--<div class="prod-logiciel-main-container default-padding">--}}
 
-    <!-- THE ELEMENT START  -->
-    <div class="prod-logiciel-container"><!-- reverse-elem : to swap img and text div -->
-        <div class="prod-logiciel-img-container">
-            <div class="prod-logiciel-img"></div>
-            <span class="prod-logiciel-lang">Vidéo disponible en anglais seulement</span>
-        </div>
-        <div class="buffer"></div>
-        <div class="prod-logiciel-text-container">
-            <h3 class="prod-logiciel-title">IRISPowerscan</h3>
-            <p class="prod-logiciel-subtitle">NUMÉRISATION ET TRAITEMENT DE DONNÉES</p>
-            <p class="prod-logiciel-text">
-                Nunc elementum venenatis dui, quis tempor nulla molestie quis. Quisque dignissim sagittis
-                lorem id blandit. Nulla facilisi. Sed blandit quam ut arcu facilisis aliquet. Orci varius
-                natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam aliquam,
-                turpis non bibendum vestibulum, odio turpis viverra massa
-            </p>
-            <a class="brochure-download-pdf-white" href="#"><?php _e('BROCHURE', THEME_TEXTDOMAIN); ?></a>
-        </div>
-    </div>
-    <!-- THE ELEMENT END  -->
+            <!-- THE ELEMENT START  -->
+            <div class="prod-logiciel-container reverse-elem"><!-- reverse-elem : to swap img and text div -->
+                <div class="prod-logiciel-img-container">
+                    <div class="prod-logiciel-img"></div>
+                    <span class="prod-logiciel-lang">Vidéo disponible en anglais seulement</span>
+                </div>
+                <div class="buffer"></div>
+                <div class="prod-logiciel-text-container">
+                    <h3 class="prod-logiciel-title">EcopyPDF Pro Office</h3>
+                    <p class="prod-logiciel-subtitle">CONVERSION DE DOCUMENTS</p>
+                    <p class="prod-logiciel-text">
+                        Nunc elementum venenatis dui, quis tempor nulla molestie quis. Quisque dignissim sagittis
+                        lorem id blandit. Nulla facilisi. Sed blandit quam ut arcu facilisis aliquet. Orci varius
+                        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam aliquam,
+                        turpis non bibendum vestibulum, odio turpis viverra massa
+                    </p>
+                    <a class="brochure-download-pdf-white" href="#"><?php _e('BROCHURE', THEME_TEXTDOMAIN); ?></a>
+                </div>
+            </div>
+            <!-- THE ELEMENT END  -->
 
-{{--</div><!-- prod-logiciel MAIN CONTAINER END -->--}}
-
-
-<!-- prod-logiciel MAIN CONTAINER START -->
-{{--<div class="prod-logiciel-main-container default-padding">--}}
-
-    <!-- THE ELEMENT START  -->
-    <div class="prod-logiciel-container reverse-elem"><!-- reverse-elem : to swap img and text div -->
-        <div class="prod-logiciel-img-container">
-            <div class="prod-logiciel-img"></div>
-            <span class="prod-logiciel-lang">Vidéo disponible en anglais seulement</span>
-        </div>
-        <div class="buffer"></div>
-        <div class="prod-logiciel-text-container">
-            <h3 class="prod-logiciel-title">EcopyPDF Pro Office</h3>
-            <p class="prod-logiciel-subtitle">CONVERSION DE DOCUMENTS</p>
-            <p class="prod-logiciel-text">
-                Nunc elementum venenatis dui, quis tempor nulla molestie quis. Quisque dignissim sagittis
-                lorem id blandit. Nulla facilisi. Sed blandit quam ut arcu facilisis aliquet. Orci varius
-                natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam aliquam,
-                turpis non bibendum vestibulum, odio turpis viverra massa
-            </p>
-            <a class="brochure-download-pdf-white" href="#"><?php _e('BROCHURE', THEME_TEXTDOMAIN); ?></a>
-        </div>
-    </div>
-    <!-- THE ELEMENT END  -->
-
-</div><!-- prod-logiciel MAIN CONTAINER END -->
+        </div><!-- prod-logiciel MAIN CONTAINER END -->
+</div>
+</div>
