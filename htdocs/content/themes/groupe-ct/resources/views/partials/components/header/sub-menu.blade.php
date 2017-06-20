17d7ae5f776ffecf94429296da7ab6bf0ba1a538
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-19 22:36:32 -0400

    Test polylang function

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/header/sub-menu.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/header/sub-menu.blade.php
@@ -9,7 +9,7 @@
         <div class="nav-sub-container row">
             <nav class="nav-sub nav-sub-left col-xs-3 col-xs-offset-2">
                 <div class="nav-section-title-container">
-                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_1_0_APPROCHE_CONSEIL) }}"><?= __('Un accompagnement sur mesure adapté à votre entreprise', GROUPE_CT) ?></a></h3>
+                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_1_0_APPROCHE_CONSEIL) }}"><?= pll__('Un accompagnement sur mesure adapté à votre entreprise', GROUPE_CT) ?></a></h3>
                 </div>
                 <ul class="nav-link-container">
                     @include('partials.components.header.sub-menu-nav-link-atom', ['page_id' => PageHelper::PAGE_1_1_ENVIRONNEMENT_PAPIER])
@@ -23,13 +23,13 @@
     <div class="sub-menu-container nl2 row hide-from-screen">
 
         <div class="sub-menu-title-container col-xs-10 col-xs-offset-2">
-            <h2 class="sub-menu-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_2_0_PRODUITS_SOLUTIONS) }}"><?= __('Des solutions qui travaillent en synergie', GROUPE_CT) ?></a></h2>
+            <h2 class="sub-menu-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_2_0_PRODUITS_SOLUTIONS) }}"><?= pll__('Des solutions qui travaillent en synergie', GROUPE_CT) ?></a></h2>
         </div>
 
         <div class="nav-sub-container row">
             <nav class="nav-sub nav-sub-left col-xs-3 col-xs-offset-2">
                 <div class="nav-section-title-container">
-                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_2_1_1_EQUIPEMENTS_BUREAU) }}"><?= __('Équipements d\'impression', GROUPE_CT) ?></a></h3>
+                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_2_1_1_EQUIPEMENTS_BUREAU) }}"><?= pll__('Équipements d\'impression', GROUPE_CT) ?></a></h3>
                 </div>
                 <ul class="nav-link-container">
                     @include('partials.components.header.sub-menu-nav-link-atom', ['page_id' => PageHelper::PAGE_2_1_1_EQUIPEMENTS_BUREAU])
@@ -40,7 +40,7 @@
             </nav>
             <nav class="nav-sub nav-sub-center col-xs-3">
                 <div class="nav-section-title-container">
-                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_2_2_1_PARC_IMPRESSION) }}"><?= __('Services d\'impression gérés', GROUPE_CT) ?></a></h3>
+                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_2_2_1_PARC_IMPRESSION) }}"><?= pll__('Services d\'impression gérés', GROUPE_CT) ?></a></h3>
                 </div>
                 <ul class="nav-link-container">
                     @include('partials.components.header.sub-menu-nav-link-atom', ['page_id' => PageHelper::PAGE_2_2_1_PARC_IMPRESSION])
@@ -51,7 +51,7 @@
             </nav>
             <nav class="nav-sub nav-sub-right col-xs-3">
                 <div class="nav-section-title-container">
-                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_2_3_1_NUMERISATION_INTELLIGENTE) }}"><?= __('Gestion électronique des documents', GROUPE_CT) ?></a></h3>
+                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_2_3_1_NUMERISATION_INTELLIGENTE) }}"><?= pll__('Gestion électronique des documents', GROUPE_CT) ?></a></h3>
                 </div>
                 <ul class="nav-link-container">
                     @include('partials.components.header.sub-menu-nav-link-atom', ['page_id' => PageHelper::PAGE_2_3_1_NUMERISATION_INTELLIGENTE])
@@ -72,15 +72,15 @@
         <div class="nav-sub-container row">
             <nav class="nav-sub nav-sub-left col-xs-3 col-xs-offset-2">
                 <div class="nav-section-title-container">
-                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}"><?= __('L\'Assistance technique CT : l\'efficacité sur toute la ligne', GROUPE_CT) ?></a></h3>
+                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}"><?= pll__('L\'Assistance technique CT : l\'efficacité sur toute la ligne', GROUPE_CT) ?></a></h3>
                 </div>
 
                 <ul class="nav-link-container">
-                    <li class="menu-item"><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}#TBD"><?= __('Accéder à Mon CT', GROUPE_CT) ?></a></li>
-                    <li class="menu-item"><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}#TBD"><?= __('Compléter une demande d’assistance', GROUPE_CT) ?></a></li>
-                    <li class="menu-item"><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}#TBD"><?= __('Commander des fournitures', GROUPE_CT) ?></a></li>
-                    <li class="menu-item"><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}#TBD"><?= __('Contacter le service à la clientèle', GROUPE_CT) ?></a></li>
-                    <li class="menu-item"><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}#TBD"><?= __('Fournir une lecture de compteurs', GROUPE_CT) ?></a></li>
+                    <li class="menu-item"><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}#TBD"><?= pll__('Accéder à Mon CT', GROUPE_CT) ?></a></li>
+                    <li class="menu-item"><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}#TBD"><?= pll__('Compléter une demande d’assistance', GROUPE_CT) ?></a></li>
+                    <li class="menu-item"><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}#TBD"><?= pll__('Commander des fournitures', GROUPE_CT) ?></a></li>
+                    <li class="menu-item"><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}#TBD"><?= pll__('Contacter le service à la clientèle', GROUPE_CT) ?></a></li>
+                    <li class="menu-item"><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_3_0_ASSISTANCE) }}#TBD"><?= pll__('Fournir une lecture de compteurs', GROUPE_CT) ?></a></li>
                 </ul>
             </nav>
         </div>
@@ -95,7 +95,7 @@
         <div class="nav-sub-container row">
             <nav class="nav-sub nav-sub-left col-xs-3 col-xs-offset-2">
                 <div class="nav-section-title-container">
-                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_0_A_PROPOS) }}"><?= __('Une entreprise à taille humaine', GROUPE_CT) ?></a></h3>
+                    <h3 class="nav-section-title"><a href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_0_A_PROPOS) }}"><?= pll__('Une entreprise à taille humaine', GROUPE_CT) ?></a></h3>
                 </div>
                 <ul class="nav-link-container">
                     @include('partials.components.header.sub-menu-nav-link-atom', ['page_id' => PageHelper::PAGE_4_1_MOT_DIRECTION])
