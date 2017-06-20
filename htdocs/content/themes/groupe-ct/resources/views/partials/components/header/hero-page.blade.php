c2927fb514b007137c71d264859b7e6bf800ad60
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-19 21:34:55 -0400

    Fix hero-page

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero-page.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero-page.blade.php
@@ -4,7 +4,10 @@
 
     <div class="hero-content-container">
         <div class="hero-title-container">
-            <h1 class="hero-title">NOS </br> <span class="red-border">PUBLICATIONS</span> </h1>
+            {{--<h1 class="hero-title">NOS </br> <span class="red-border">PUBLICATIONS</span></h1>--}}
+            @loop
+                <h1 class="hero-title">{{ Loop::title() }}</h1>
+            @endloop
         </div>
     </div>
 
