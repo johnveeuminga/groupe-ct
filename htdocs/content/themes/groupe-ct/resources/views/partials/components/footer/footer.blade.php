c4dd1f1a3026fb2fc8b17dfdc26f253aa06cccb1
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-20 13:20:00 -0400

    Bugfix footer

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/footer/footer.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/footer/footer.blade.php
@@ -68,7 +68,7 @@
             </div>
             <div class="legal-container">
                 <p>© <?php echo date("Y"); ?> Groupe C.T Tous droits réservé</p>
-                <a href="#">{{  pll__('Mentions légales'); }}</a>
+                <a href="#">{{ pll__('Mentions légales') }}</a>
             </div>
 
 
