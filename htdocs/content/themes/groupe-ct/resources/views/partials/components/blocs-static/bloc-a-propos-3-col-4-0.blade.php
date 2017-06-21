0917ab58019fdf4e5343b72527cdc920fa563db6
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-21 14:41:40 -0400

    Bloc contact 4.0 and 4.7

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-a-propos-3-col-4-0.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-a-propos-3-col-4-0.blade.php
@@ -6,7 +6,7 @@
                 <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-historique.png" alt="">
             </div>
             <h2 class="contact-title">{{ pll__('À l\'avant-garde depuis 1987') }}</h2>
-            <p class="contact-text">{!! nl2br('Contact a propos col 1') !!}</p>
+            <p class="contact-text">{!! nl2br(pll__('Contact a propos col 1')) !!}</p>
             <a class="contact-link-cta" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_3_HISTORIQUE) }}"><img class="contact-cta" src="{{ themosis_assets() }}/images/icon/icon-cta-arrow-white.png" alt=""></a>
         </div>
     </div>
@@ -17,7 +17,7 @@
                 <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-equipe.png" alt="">
             </div>
             <h2 class="contact-title">{{ pll__('Une équipe engagée') }}</h2>
-            <p class="contact-text">{!! nl2br('Contact a propos col 2') !!}</p>
+            <p class="contact-text">{!! nl2br(pll__('Contact a propos col 2')) !!}</p>
             <a class="contact-link-cta" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_4_EQUIPE) }}"><img class="contact-cta" src="{{ themosis_assets() }}/images/icon/icon-cta-arrow-white.png" alt=""></a>
         </div>
     </div>
@@ -28,7 +28,7 @@
                 <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-divisions.png" alt="">
             </div>
             <h2 class="contact-title">{{ pll__('Nos sociétés') }}</h2>
-            <p class="contact-text">{!! nl2br('Contact a propos col 3') !!}</p>
+            <p class="contact-text">{!! nl2br(pll__('Contact a propos col 3')) !!}</p>
             <a class="contact-link-cta" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_2_DIVISIONS) }}"><img class="contact-cta" src="{{ themosis_assets() }}/images/icon/icon-cta-arrow-white.png" alt=""></a>
         </div>
     </div>
