a75e9bcb688a88dfdd3caf35dee41cadff8c57bf
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-21 14:25:14 -0400

    Bloc contact 4.0 and 4.7

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-a-propos-3-col-4-0.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-a-propos-3-col-4-0.blade.php
@@ -2,7 +2,9 @@
 
     <div class="contact-container">
         <div class="inner-container">
-            <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-historique.png" alt="">
+            <div class="img-container">
+                <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-historique.png" alt="">
+            </div>
             <h2 class="contact-title">{{ pll__('À l\'avant-garde depuis 1987') }}</h2>
             <p class="contact-text">{!! nl2br('Contact a propos col 1') !!}</p>
             <a class="contact-link-cta" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_3_HISTORIQUE) }}"><img class="contact-cta" src="{{ themosis_assets() }}/images/icon/icon-cta-arrow-white.png" alt=""></a>
@@ -11,7 +13,9 @@
 
     <div class="contact-container">
         <div class="inner-container">
-            <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-equipe.png" alt="">
+            <div class="img-container">
+                <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-equipe.png" alt="">
+            </div>
             <h2 class="contact-title">{{ pll__('Une équipe engagée') }}</h2>
             <p class="contact-text">{!! nl2br('Contact a propos col 2') !!}</p>
             <a class="contact-link-cta" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_4_EQUIPE) }}"><img class="contact-cta" src="{{ themosis_assets() }}/images/icon/icon-cta-arrow-white.png" alt=""></a>
@@ -20,7 +24,9 @@
 
     <div class="contact-container">
         <div class="inner-container">
-            <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-divisions.png" alt="">
+            <div class="img-container">
+                <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-divisions.png" alt="">
+            </div>
             <h2 class="contact-title">{{ pll__('Nos sociétés') }}</h2>
             <p class="contact-text">{!! nl2br('Contact a propos col 3') !!}</p>
             <a class="contact-link-cta" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_2_DIVISIONS) }}"><img class="contact-cta" src="{{ themosis_assets() }}/images/icon/icon-cta-arrow-white.png" alt=""></a>
--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-a-propos-3-col-4-7.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-a-propos-3-col-4-7.blade.php
@@ -2,7 +2,9 @@
 
     <div class="contact-container">
         <div class="inner-container">
-            <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-help.png" alt="">
+            <div class="img-container">
+                <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-help.png" alt="">
+            </div>
             <h2 class="contact-title">{{ pll__('Demander conseil') }}</h2>
             <p class="contact-text">{!! nl2br('Contact nous joindre col 1') !!}</p>
             <a class="contact-link-cta" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_3_HISTORIQUE) }}"><img class="contact-cta" src="{{ themosis_assets() }}/images/icon/icon-cta-arrow-white.png" alt=""></a>
@@ -11,7 +13,9 @@
 
     <div class="contact-container">
         <div class="inner-container">
-            <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-assist.png" alt="">
+            <div class="img-container">
+                <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-assist.png" alt="">
+            </div>
             <h2 class="contact-title">{{ pll__('Assistance technique') }}</h2>
             <p class="contact-text">{!! nl2br('Contact nous joindre col 2') !!}</p>
             <a class="contact-link-cta" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_4_EQUIPE) }}"><img class="contact-cta" src="{{ themosis_assets() }}/images/icon/icon-cta-arrow-white.png" alt=""></a>
@@ -20,7 +24,9 @@
 
     <div class="contact-container">
         <div class="inner-container">
-            <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-fourniture.png" alt="">
+            <div class="img-container">
+                <img class="contact-icon" src="{{ themosis_assets() }}/images/icon/icon-fourniture.png" alt="">
+            </div>
             <h2 class="contact-title">{{ pll__('Commande de fournitures') }}</h2>
             <p class="contact-text">{!! nl2br('Contact nous joindre col 3') !!}</p>
             <a class="contact-link-cta" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_4_2_DIVISIONS) }}"><img class="contact-cta" src="{{ themosis_assets() }}/images/icon/icon-cta-arrow-white.png" alt=""></a>
