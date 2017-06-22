8236c16874d57d52de1d8b42fcf6156b5e6b5fa0
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-21 21:16:54 -0400

    Fix lego 3

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_3.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_3.blade.php
@@ -28,6 +28,6 @@
                 @endif
             </div>
         @endif
-        @if ($slide['bloc_3_cta_link'])<a class="btn-bloc2 primary-btn-blue" href="{{ $block->get_field('bloc_3_cta_link') }}" {{ $$block->get_field('bloc_3_cta_blank') ? "target='_blank'" : "" }}>{{ $block->get_field('bloc_3_cta_label') }}</a>@endif
+        @if ($block->get_field('bloc_3_cta_link'))<a class="btn-bloc2 primary-btn-blue" href="{{ $block->get_field('bloc_3_cta_link') }}" {{ $$block->get_field('bloc_3_cta_blank') ? "target='_blank'" : "" }}>{{ $block->get_field('bloc_3_cta_label') }}</a>@endif
     </div>
 </div>
