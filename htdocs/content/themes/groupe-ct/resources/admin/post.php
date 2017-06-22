5b6a622a9f255a18bdfe8233a31bd190b788c02d
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-21 22:10:57 -0400

    Added image size instructions

--- htdocs/content/themes/groupe-ct/resources/admin/post.php
+++ htdocs/content/themes/groupe-ct/resources/admin/post.php
@@ -8,7 +8,7 @@ acf_add_local_field_group(array (
     'fields' => array (
         $acf_fields->generate_true_false('post_show_date', __('Show Date', GROUPE_CT)),
         $acf_fields->generate_true_false('post_is_featured', __('Is featured?', GROUPE_CT)),
-        $acf_fields->generate_image('post_featured_image', __('Featured image', GROUPE_CT), ''),
+        $acf_fields->generate_image('post_featured_image', __('Featured image', GROUPE_CT), __('Minimum Width : 585px | Minimum Height : 590px', 'GROUPE-CT')),
     ),
     'location' => array (
         array (
