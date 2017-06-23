d84fec4d0f0d57263d067ec42521ae1aeb1d34d4
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-23 19:26:51 -0400

    Fix css bloc 08

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_8.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_8.blade.php
@@ -14,8 +14,8 @@
                 <div class="wys-content">{!! $collapse['bloc_8_content'] !!}</div>
             </div>
         </div>
-        @foreach ($block['bloc_8_blocks'] as $blocks)
-            <?php var_dump($blocks) ?>
-        @endforeach
+{{--        @foreach ($block['bloc_8_blocks'] as $blocks)--}}
+<!--            --><?php //var_dump($blocks) ?>
+        {{--@endforeach--}}
     @endforeach
 </div>
