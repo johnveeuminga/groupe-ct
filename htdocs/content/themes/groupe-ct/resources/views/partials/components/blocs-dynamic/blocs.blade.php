6dea11891386145ec221037844fc4182ff2bc8c1
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2017-06-23 19:43:56 -0400

    block 15 plugged

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/blocs.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/blocs.blade.php
@@ -1,5 +1,4 @@
 <section id="blocs" class="blocs-main-container">
-    @include('partials.components.blocs-dynamic.bloc_15')
     @include('partials.components.blocs-dynamic.bloc_16')
     @include('partials.components.blocs-dynamic.bloc_17')
     @include('partials.components.blocs-dynamic.bloc_18')
