12ce88e38d8b89baf89e2ff22b3ede32616c880b
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-23 17:12:37 -0400

    Added newsletter to footer by default

--- htdocs/content/themes/groupe-ct/resources/views/templates/footer.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/templates/footer.blade.php
@@ -2,6 +2,7 @@
 <section id="footer" class="footer-main-container container-fluid">
 
     <!--    INCLUDE FOOTER COMPONENTS -->
+    @include('partials.components.blocs-static.bloc-newsletter')
     @include('partials.components.footer.footer')
 
 </section>
