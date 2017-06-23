9a0d655008db98ecc096a7c973e79720e53ed3ce
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-23 12:56:23 -0400

    HTML page header

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero-page.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero-page.blade.php
@@ -4,9 +4,8 @@
 
     <div class="hero-content-container">
         <div class="hero-title-container">
-            {{--<h1 class="hero-title">NOS </br> <span class="red-border">PUBLICATIONS</span></h1>--}}
             @loop
-                <h1 class="hero-title">@if(get_field('page_html_title', Loop::id()) && !empty(get_field('page_html_title', Loop::id()))){{ get_field('page_html_title', Loop::id()) }}@else{{ Loop::title() }}@endif</h1>
+            <h1 class="hero-title">@if(get_field('page_html_title', Loop::id()) && !empty(get_field('page_html_title', Loop::id()))){!! get_field('page_html_title', Loop::id())  !!} @else<span class="red-border">{{ Loop::title() }}</span>@endif</h1>
             @endloop
         </div>
     </div>
