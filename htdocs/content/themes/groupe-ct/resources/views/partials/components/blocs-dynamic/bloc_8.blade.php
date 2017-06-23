da90d9dc326bdd195a329bcd697dffe3e5135433
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-23 19:54:46 -0400

    fix block 08 css

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_8.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_8.blade.php
@@ -12,17 +12,17 @@
             </div>
             <div class="toggle-content">
                 <div class="wys-content">{!! $collapse['bloc_8_content'] !!}</div>
+                @if (isset($collapse['bloc_8_blocks']))
+                    @foreach ($collapse['bloc_8_blocks'] as $col)
+                        @while ( has_sub_field('bloc_flexible_content', $col['bloc_8_block']->ID) )
+                            <?php
+                            $block = new \Theme\Models\Bloc(get_row_layout());
+                            ?>
+                            @include($block->load_path())
+                        @endwhile
+                    @endforeach
+                @endif
             </div>
-            @if (isset($collapse['bloc_8_blocks']))
-                @foreach ($collapse['bloc_8_blocks'] as $col)
-                    @while ( has_sub_field('bloc_flexible_content', $col['bloc_8_block']->ID) )
-                        <?php
-                        $block = new \Theme\Models\Bloc(get_row_layout());
-                        ?>
-                        @include($block->load_path())
-                    @endwhile
-                @endforeach
-            @endif
         </div>
     @endforeach
 </div>
