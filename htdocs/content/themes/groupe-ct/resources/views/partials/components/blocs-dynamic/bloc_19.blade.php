102c926955298d0189eade94939415073ac5ed16
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-23 16:41:33 -0400

    Fix subheader square

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_19.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_19.blade.php
@@ -16,7 +16,7 @@ $bloc_19_center_description = isset($bloc_19_center_description) ? $bloc_19_cent
     <div class="section-title-container">
 
         {{--@if(isset($bloc_19_subheader))--}}
-            <li class="{{ $bloc_19_subheader_carret === true ? 'upper-title' : 'upper-title-no-dot' }} {{ $bloc_19_center_subheader === true ? 'align-center' : '' }}">{{ $bloc_19_subheader }}</li> <!-- upper-title-no-dot : enleve le list style -->
+            <p class="{{ $bloc_19_subheader_carret === true ? 'upper-title' : 'upper-title-no-dot' }} {{ $bloc_19_center_subheader === true ? 'align-center' : '' }}">{{ $bloc_19_subheader }}</p> <!-- upper-title-no-dot : enleve le list style -->
         {{--@endif--}}
 
         {{--@if(isset($bloc_19_title))--}}
