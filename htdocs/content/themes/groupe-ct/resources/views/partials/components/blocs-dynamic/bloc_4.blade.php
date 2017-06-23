1c83d42a6ca0cb09d4d0e5aa25f02c96789945fd
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-23 11:31:04 -0400

    Fix dynamic bloc 4

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_4.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_4.blade.php
@@ -1,111 +1,30 @@
 <div class="four-col-icons-main-container default-padding default-width">
-
     <div class="four-col-icons-container">
-        <div class="four-col-icons-item col-xs-12 col-sm-6 col-md-3">
-            <div class="four-col-icons-img-container">
-                <img class="four-col-icons-img" src="{{ themosis_assets() }}/images/icon/icon-bars-circle.png" alt="" >
-                <img class="four-col-icons-img" src="{{ themosis_assets() }}/images/icon/icon-bars.png " alt="" >
-            </div>
-
-            <h3 class="four-col-icons-title">Efficacité <br>
-                d’entreprise</h3>
-            <p class="four-col-icons-desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-        </div>
-        <div class="four-col-icons-item col-xs-12 col-sm-6 col-md-3">
-            <div class="four-col-icons-img-container">
-                <img class="four-col-icons-img" src="{{ themosis_assets() }}/images/icon/icon-dollar-circle.png" alt="" >
-                <img class="four-col-icons-img" src="{{ themosis_assets() }}/images/icon/icon-dollar_v2.png " alt="" >
-            </div>
-
-            <h3 class="four-col-icons-title">Efficacité<br>
-                d’entreprise</h3>
-            <p class="four-col-icons-desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-        </div>
-        <div class="four-col-icons-item col-xs-12 col-sm-6 col-md-3">
-            <div class="four-col-icons-img-container">
-                <img class="four-col-icons-img" src="{{ themosis_assets() }}/images/icon/icon-lock-circle.png" alt="" >
-                <img class="four-col-icons-img" src="{{ themosis_assets() }}/images/icon/icon-lock.png" alt="" >
-            </div>
-
-            <h3 class="four-col-icons-title">Efficacité<br>
-                d’entreprise</h3>
-            <p class="four-col-icons-desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-        </div>
-        <div class="four-col-icons-item col-xs-12 col-sm-6 col-md-3">
-            <div class="four-col-icons-img-container">
-                <img class="four-col-icons-img" src="{{ themosis_assets() }}/images/icon/icon-paper-stack-circle.png" alt="" >
-                <img class="four-col-icons-img" src="{{ themosis_assets() }}/images/icon/icon-paper-stack.png" alt="" >
-            </div>
-
-            <h3 class="four-col-icons-title">Efficacité<br>
-                d’entreprise</h3>
-            <p class="four-col-icons-desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-        </div>
+        @include('partials.components.helpers.bloc-4-single-icon', [
+            'icon' => $block->get_field('bloc_4_1_icon')['url'],
+            'title' => $block->get_field('bloc_4_1_title'),
+            'description' => $block->get_field('bloc_4_1_description'),
+        ])
+        @include('partials.components.helpers.bloc-4-single-icon', [
+            'icon' => $block->get_field('bloc_4_2_icon')['url'],
+            'title' => $block->get_field('bloc_4_2_title'),
+            'description' => $block->get_field('bloc_4_2_description'),
+        ])
+        @include('partials.components.helpers.bloc-4-single-icon', [
+            'icon' => $block->get_field('bloc_4_3_icon')['url'],
+            'title' => $block->get_field('bloc_4_3_title'),
+            'description' => $block->get_field('bloc_4_3_description'),
+        ])
+        @include('partials.components.helpers.bloc-4-single-icon', [
+            'icon' => $block->get_field('bloc_4_4_icon')['url'],
+            'title' => $block->get_field('bloc_4_4_title'),
+            'description' => $block->get_field('bloc_4_4_description'),
+        ])
     </div>
 
-    <div class="btn-container">
-        <a class="primary-btn-blue" href="#"><?php _e('En savoir plus', THEME_TEXTDOMAIN); ?></a>
-    </div>
-
-
-</div>
-{{--<div class="four-col-icons-main-container default-padding default-width">--}}
-
-    {{--<div class="four-col-icons-container">--}}
-        {{--<div class="four-col-icons-item col-xs-12 col-sm-6 col-md-3">--}}
-            {{--<div class="four-col-icons-img-container">--}}
-                {{--<!-- FIXME Two variants of icon not in specs?! -->--}}
-                {{--<img class="four-col-icons-img" src="{{ $block->get_field('bloc_4_1_icon')['url'] }}" alt="{{ $block->get_field('bloc_4_1_icon')['alt'] }}">--}}
-                {{--<img class="four-col-icons-img" src="{{ $block->get_field('bloc_4_1_icon')['url'] }}" alt="{{ $block->get_field('bloc_4_1_icon')['alt'] }}">--}}
-            {{--</div>--}}
-
-            {{--<h3 class="four-col-icons-title">{{ $block->get_field('bloc_4_1_title') }}</h3>--}}
-            {{--<p class="four-col-icons-desc">{{ $block->get_field('bloc_4_1_description') }}</p>--}}
-        {{--</div>--}}
-        {{--<div class="four-col-icons-item col-xs-12 col-sm-6 col-md-3">--}}
-            {{--<div class="four-col-icons-img-container">--}}
-                {{--<img class="four-col-icons-img" src="{{ $block->get_field('bloc_4_2_icon')['url'] }}" alt="{{ $block->get_field('bloc_4_2_icon')['alt'] }}">--}}
-                {{--<img class="four-col-icons-img" src="{{ $block->get_field('bloc_4_2_icon')['url'] }}" alt="{{ $block->get_field('bloc_4_2_icon')['alt'] }}">--}}
-            {{--</div>--}}
-
-            {{--<h3 class="four-col-icons-title">{{ $block->get_field('bloc_4_2_title') }}</h3>--}}
-            {{--<p class="four-col-icons-desc">{{ $block->get_field('bloc_4_2_description') }}</p>--}}
-        {{--</div>--}}
-        {{--<div class="four-col-icons-item col-xs-12 col-sm-6 col-md-3">--}}
-            {{--<div class="four-col-icons-img-container">--}}
-                {{--<img class="four-col-icons-img" src="{{ $block->get_field('bloc_4_3_icon')['url'] }}" alt="{{ $block->get_field('bloc_4_3_icon')['alt'] }}">--}}
-                {{--<img class="four-col-icons-img" src="{{ $block->get_field('bloc_4_3_icon')['url'] }}" alt="{{ $block->get_field('bloc_4_3_icon')['alt'] }}">--}}
-            {{--</div>--}}
-
-            {{--<h3 class="four-col-icons-title">{{ $block->get_field('bloc_4_3_title') }}</h3>--}}
-            {{--<p class="four-col-icons-desc">{{ $block->get_field('bloc_4_3_description') }}</p>--}}
-        {{--</div>--}}
-        {{--<div class="four-col-icons-item col-xs-12 col-sm-6 col-md-3">--}}
-            {{--<div class="four-col-icons-img-container">--}}
-                {{--<img class="four-col-icons-img" src="{{ $block->get_field('bloc_4_4_icon')['url'] }}" alt="{{ $block->get_field('bloc_4_4_icon')['alt'] }}">--}}
-                {{--<img class="four-col-icons-img" src="{{ $block->get_field('bloc_4_4_icon')['url'] }}" alt="{{ $block->get_field('bloc_4_4_icon')['alt'] }}">--}}
-            {{--</div>--}}
-
-            {{--<h3 class="four-col-icons-title">{{ $block->get_field('bloc_4_4_title') }}</h3>--}}
-            {{--<p class="four-col-icons-desc">{{ $block->get_field('bloc_4_4_description') }}</p>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-
+    <!-- FIXME missing global cta -->
     {{--<div class="btn-container">--}}
         {{--<a class="primary-btn-blue" href="{{ $block->get_field('bloc_4_1_cta_link') }}" {{ $block->get_field('bloc_4_1_cta_blank') ? 'target="blank"' : '' }}>{{ $block->get_field('bloc_4_1_cta_label') }}</a>--}}
     {{--</div>--}}
 
-
-{{--</div>--}}
+</div>
---
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/helpers/bloc-4-single-icon.blade.php
@@ -0,0 +1,12 @@
+@if (!empty($icon) || !empty($title) || !empty($description))
+    <div class="four-col-icons-item col-xs-12 col-sm-6 col-md-3">
+        @if (!empty($icon))
+            <div class="four-col-icons-img-container">
+                <img class="four-col-icons-img" src="{{ $icon }}">
+            </div>
+        @endif
+
+        @if (!empty($title))<h3 class="four-col-icons-title">{{ $title }}</h3>@endif
+        @if (!empty($description))<p class="four-col-icons-desc">{{ $description}}</p>@endif
+    </div>
+@endif
