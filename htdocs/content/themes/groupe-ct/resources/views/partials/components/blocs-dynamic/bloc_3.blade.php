667ebc50fc533a9926e7e425891d57e52d227829
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2017-06-24 11:09:44 +0000

    fix bloc 3 paragraphs

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_3.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_3.blade.php
@@ -8,7 +8,7 @@
     <div class="text-container">
         @if (!empty($block->get_field('bloc_3_subheader')))<p class="upper-title">{{ $block->get_field('bloc_3_subheader') }}</p>@endif
         @if (!empty($block->get_field('bloc_3_title')))<h2 class="text-container-title">{{ $block->get_field('bloc_3_title') }}</h2>@endif
-        @if (!empty($block->get_field('bloc_3_text')))<p class="text">{{ wpautop($block->get_field('bloc_3_text')) }}</p>@endif
+        @if (!empty($block->get_field('bloc_3_text')))<p class="text">{!! wpautop($block->get_field('bloc_3_text')) !!}</p>@endif
         @if (!empty($block->get_field('bloc_3_phone_1')) || !empty($block->get_field('bloc_3_phone_2')))
             <div class="contact-container">
                 @if (!empty($block->get_field('bloc_3_phone_1')))
