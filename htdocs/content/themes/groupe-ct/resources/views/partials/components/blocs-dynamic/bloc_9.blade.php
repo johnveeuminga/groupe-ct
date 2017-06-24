1e8e789a913412bd79417cd86bf4e7000bd0799b
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-24 15:58:02 -0400

    Fix bloc 9 allignment

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_9.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_9.blade.php
@@ -7,11 +7,11 @@
             @if (!empty($block->get_field('bloc_9_1_percentage')))
                 <div class="chart-container">
                     <div class="chart">
-                        <canvas class="doughnut-chart01 doughnut-chart" data-initiated="false" data-description="{{ $block->get_field('bloc_9_1_description') }}" data-unfilled="{{ $block->get_field('bloc_9_1_unfilled') }}" data-primary="{{ $block->get_field('bloc_9_1_primary') }}" data-percentage="{{ $block->get_field('bloc_9_1_percentage') }}" data-label="{{ $block->get_field('bloc_9_1_label') }}"></canvas>
+                        <canvas class="doughnut-chart01 doughnut-chart" data-initiated="false" data-description="{!! $block->get_field('bloc_9_1_description') !!}" data-unfilled="{!! $block->get_field('bloc_9_1_unfilled') !!}" data-primary="{!! $block->get_field('bloc_9_1_primary') !!}" data-percentage="{!! $block->get_field('bloc_9_1_percentage') !!}" data-label="{!! $block->get_field('bloc_9_1_label') !!}"></canvas>
                     </div>
                     <div class="chart-text">
-                        <p>{{ $block->get_field('bloc_9_1_description') }}</p>
-                        <span>{{ $block->get_field('bloc_9_1_source') }}</span>
+                        <p>{!! $block->get_field('bloc_9_1_description') !!}</p>
+                        <span>{!! $block->get_field('bloc_9_1_source') !!}</span>
                     </div>
                 </div>
             @endif
@@ -21,11 +21,11 @@
             @if (!empty($block->get_field('bloc_9_2_percentage')))
                 <div class="chart-container">
                     <div class="chart">
-                        <canvas class="doughnut-chart02 doughnut-chart" data-initiated="false" data-description="{{ $block->get_field('bloc_9_2_description') }}" data-unfilled="{{ $block->get_field('bloc_9_2_unfilled') }}" data-primary="{{ $block->get_field('bloc_9_2_primary') }}" data-percentage="{{ $block->get_field('bloc_9_2_percentage') }}" data-label="{{ $block->get_field('bloc_9_2_label') }}"></canvas>
+                        <canvas class="doughnut-chart02 doughnut-chart" data-initiated="false" data-description="{!! $block->get_field('bloc_9_2_description') !!}" data-unfilled="{!! $block->get_field('bloc_9_2_unfilled') !!}" data-primary="{!! $block->get_field('bloc_9_2_primary') !!}" data-percentage="{!! $block->get_field('bloc_9_2_percentage') !!}" data-label="{!! $block->get_field('bloc_9_2_label') !!}"></canvas>
                     </div>
                     <div class="chart-text">
-                        <p>{{ $block->get_field('bloc_9_2_description') }}</p>
-                        <span>{{ $block->get_field('bloc_9_2_source') }}</span>
+                        <p>{!! $block->get_field('bloc_9_2_description') !!}</p>
+                        <span>{!! $block->get_field('bloc_9_2_source')  !!}</span>
                     </div>
                 </div>
             @endif
