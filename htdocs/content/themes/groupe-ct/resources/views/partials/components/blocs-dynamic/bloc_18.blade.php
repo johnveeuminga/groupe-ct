8470785dae12a9b1f84490f969732db503694606
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-24 14:13:01 -0400

    Fix bloc 18

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_18.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_18.blade.php
@@ -2,23 +2,28 @@
 <div class="toggle-content-main-container row default-padding">
     <div class="col-xs-12">
 
+        <h2 class="text-bloc-main-title">{!! $block->get_field('bloc_18_title') !!}</h2>
         <!-- THE ELEMENT START  -->
         <div class="text-bloc-main-container default-width">
             <div class="text-bloc-container">
                 <h3 class="text-bloc-title">{!! $block->get_field('bloc_18_1_title') !!}</h3>
                 <p class="text-bloc-text">{!! $block->get_field('bloc_18_1_description') !!}</p>
+                <a class="cta-see-more" href="{{ $block->get_field('bloc_18_1_cta_link') }}" {{ $block->get_field('bloc_18_1_cta_blank') ? 'target="blank"' : '' }}>{{ $block->get_field('bloc_18_1_cta_label') }}</a>
             </div>
             <div class="text-bloc-container">
                 <h3 class="text-bloc-title">{!! $block->get_field('bloc_18_2_title') !!}</h3>
                 <p class="text-bloc-text">{!! $block->get_field('bloc_18_2_description') !!}</p>
+                <a class="cta-see-more" href="{{ $block->get_field('bloc_18_2_cta_link') }}" {{ $block->get_field('bloc_18_2_cta_blank') ? 'target="blank"' : '' }}>{{ $block->get_field('bloc_18_2_cta_label') }}</a>
             </div>
             <div class="text-bloc-container">
                 <h3 class="text-bloc-title">{!! $block->get_field('bloc_18_3_title') !!}</h3>
                 <p class="text-bloc-text">{!! $block->get_field('bloc_18_3_description') !!}</p>
+                <a class="cta-see-more" href="{{ $block->get_field('bloc_18_3_cta_link') }}" {{ $block->get_field('bloc_18_3_cta_blank') ? 'target="blank"' : '' }}>{{ $block->get_field('bloc_18_3_cta_label') }}</a>
             </div>
             <div class="text-bloc-container">
                 <h3 class="text-bloc-title">{!! $block->get_field('bloc_18_4_title') !!}</h3>
                 <p class="text-bloc-text">{!! $block->get_field('bloc_18_4_description') !!}</p>
+                <a class="cta-see-more" href="{{ $block->get_field('bloc_18_4_cta_link') }}" {{ $block->get_field('bloc_18_4_cta_blank') ? 'target="blank"' : '' }}>{{ $block->get_field('bloc_18_4_cta_label') }}</a>
             </div>
         </div> <!-- TEXT BLOC MAIN CONTAINER END -->
         <!-- THE ELEMENT END  -->
