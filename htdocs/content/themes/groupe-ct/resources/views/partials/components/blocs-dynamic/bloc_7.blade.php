de890d7e8b7e0b6455eb9d58d3763bd12efdbb80
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-24 00:12:59 -0400

    Fix chart

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_7.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_7.blade.php
@@ -6,16 +6,16 @@
                 <img class="two-col-icons-text-img" src="{{ $row['bloc_7_col_1_icon']['url'] }}" alt="{{ $row['bloc_7_col_1_icon']['alt'] }}">
             </div>
 
-            <h3 class="two-col-icons-text-title">{{ $row['bloc_7_col_1_title'] }}</h3>
-            <p class="two-col-icons-text-desc">{{ $row['bloc_7_col_1_description'] }}</p>
+            <h3 class="two-col-icons-text-title">{!! $row['bloc_7_col_1_title'] !!} </h3>
+            <p class="two-col-icons-text-desc">{!! $row['bloc_7_col_1_description'] !!}</p>
         </div>
         <div class="two-col-icons-text-item col-xs-12 col-sm-6">
             <div class="two-col-icons-text-img-container">
                 <img class="two-col-icons-text-img" src="{{ $row['bloc_7_col_2_icon']['url'] }}" alt="{{ $row['bloc_7_col_2_icon']['alt'] }}">
             </div>
 
-            <h3 class="two-col-icons-text-title">{{ $row['bloc_7_col_2_title'] }}</h3>
-            <p class="two-col-icons-text-desc">{{ $row['bloc_7_col_2_description'] }}</p>
+            <h3 class="two-col-icons-text-title">{!! $row['bloc_7_col_2_title'] !!} </h3>
+            <p class="two-col-icons-text-desc">{!! $row['bloc_7_col_2_description'] !!}</p>
         </div>
     @endforeach
 </div>
