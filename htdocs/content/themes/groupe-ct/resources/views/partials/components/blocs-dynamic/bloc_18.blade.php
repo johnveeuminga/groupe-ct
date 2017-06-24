15401dd3ddcb5844bfc69305360356d3c3eb157a
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-24 14:04:34 -0400

    Fix bloc 18

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_18.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_18.blade.php
@@ -5,22 +5,18 @@
         <!-- THE ELEMENT START  -->
         <div class="text-bloc-main-container default-width">
             <div class="text-bloc-container">
-                <div class="bloc-top-line"></div>
                 <h3 class="text-bloc-title">{{ $block->get_field('bloc_18_1_title') }}</h3>
                 <p class="text-bloc-text">{{ $block->get_field('bloc_18_1_description') }}</p>
             </div>
             <div class="text-bloc-container">
-                <div class="bloc-top-line"></div>
                 <h3 class="text-bloc-title">{{ $block->get_field('bloc_18_2_title') }}</h3>
                 <p class="text-bloc-text">{{ $block->get_field('bloc_18_2_description') }}</p>
             </div>
             <div class="text-bloc-container">
-                <div class="bloc-top-line"></div>
                 <h3 class="text-bloc-title">{{ $block->get_field('bloc_18_3_title') }}</h3>
                 <p class="text-bloc-text">{{ $block->get_field('bloc_18_3_description') }}</p>
             </div>
             <div class="text-bloc-container">
-                <div class="bloc-top-line"></div>
                 <h3 class="text-bloc-title">{{ $block->get_field('bloc_18_4_title') }}</h3>
                 <p class="text-bloc-text">{{ $block->get_field('bloc_18_4_description') }}</p>
             </div>
