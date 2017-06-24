326d5c395e51423a2b19f18c3487f3b8c014318c
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2017-06-24 14:21:34 +0000

    bugfix block 15 member picture display

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_15.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_15.blade.php
@@ -16,7 +16,7 @@
             <div class="team-info-container">
                 {{-- BLUE BAR WITH TITLE END --}}
                 <div class="team-info-inner-container">
-                    <img class="team-info-img" src="{{ get_the_post_thumbnail_url($member, 'full') }}" alt="{{ get_the_post_thumbnail_caption($member) }}">
+                    <img class="team-info-img" src="{{ get_the_post_thumbnail_url($member['block_15_team_member'], 'full') }}" alt="{{ get_the_post_thumbnail_caption($member['block_15_team_member']) }}">
                     <h3 class="team-info-name">{{ $member['bloc_15_team_member']->post_title }}</h3>
                     <p class="info team-info-title">{{ $member['bloc_15_team_member']->team_member_title }}</p>
                     <a class="info team-info-phone" href="tel:{{ $member['bloc_15_team_member']->team_member_phone }}"><span>T :</span>{{ $member['bloc_15_team_member']->team_member_phone }} <?php _e('poste:', 'GROUPE-CT'); ?> {{ $member['bloc_15_team_member']->team_member_position }}</a>
