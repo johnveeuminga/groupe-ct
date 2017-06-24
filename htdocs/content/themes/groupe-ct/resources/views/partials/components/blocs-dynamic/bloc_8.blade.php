17e8c48a52e9c0eb9aeb9d5041fcf67269707093
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-23 22:19:39 -0400

    fix block 08 css

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_8.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_8.blade.php
@@ -1,16 +1,16 @@
 <div class="toggle-content-main-container default-padding default-width">
     @foreach ($block->get_field('bloc_8_collapse') as $collapse)
-        <div class="toggle-content-container @if($collapse['bloc_8_active']) active-header @endif">
-            <div class="toggle-content-header">
+        <div class="toggle-content-container">
+            <div class="toggle-content-header @if($collapse['bloc_8_active']) active-header @endif">
                 <div class="title-container">
                     <img class="title-icon" src="{{ $collapse['bloc_8_icon']['url'] }}" alt="{{ $collapse['bloc_8_icon']['alt'] }}">
                     <h3 class="toggle-content-title" href="">{{ $collapse['bloc_8_title'] }}</h3>
                 </div>
-                <img class="toggle-content-carret" src="{{ themosis_assets() }}/images/icon/icon-chevron-white.png" alt="">
-                <div class="triangle-blue"></div>
+                <img class="toggle-content-carret @if($collapse['bloc_8_active']) toggle-carret @endif" src="{{ themosis_assets() }}/images/icon/icon-chevron-white.png" alt="">
+                <div class="triangle-blue @if($collapse['bloc_8_active']) toggle-triangle @endif"></div>
                 <a class="open-toggle-content" href="#"></a>
             </div>
-            <div class="toggle-content">
+            <div class="toggle-content @if($collapse['bloc_8_active']) open-content @endif">
                 <div class="wys-content">
                     {!! $collapse['bloc_8_content'] !!}
                     @if (isset($collapse['bloc_8_blocks']))
