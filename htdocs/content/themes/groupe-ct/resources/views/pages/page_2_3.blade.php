9ede39cef72bb100bbeb76b5c02b16dff18f0dcb
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-25 17:22:15 -0400

    Fix template 2.2 2.3

--- htdocs/content/themes/groupe-ct/resources/views/pages/page_2_3.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/pages/page_2_3.blade.php
@@ -1,27 +1,31 @@
 @extends('templates.main')
 
 @section('main')
-    <div class="container-fluid">
-        @include('partials.components.blocs-static.bloc-tab-products-2-3')
+    <div class="container-fluid" id="_main-container-page-{{ get_the_ID() }}">
+        <div class="bloc-product-search-main-container">
+            <div class="bloc-product-outer-container">
+                @include('partials.components.blocs-static.bloc-tab-products-2-3')
 
-        @if( have_rows('blocks') )
+                @if( have_rows('blocks') )
 
-            @while ( have_rows('blocks') )
-                <?php the_row(); ?>
+                    @while ( have_rows('blocks') )
+                        <?php the_row(); ?>
 
-                <?php
-                    $row = get_row();
-                    $row_id = array_shift($row);
-                ?>
-                @while ( has_sub_field('bloc_flexible_content', $row_id) )
-                    <?php
-                        $block = new \Theme\Models\Bloc(get_row_layout());
-                    ?>
-                    @include($block->load_path(), ['block' => $block])
-                @endwhile
+                        <?php
+                            $row = get_row();
+                            $row_id = array_shift($row);
+                        ?>
+                        @while ( has_sub_field('bloc_flexible_content', $row_id) )
+                            <?php
+                                $block = new \Theme\Models\Bloc(get_row_layout());
+                            ?>
+                            @include($block->load_path(), ['block' => $block])
+                        @endwhile
 
-            @endwhile
+                    @endwhile
 
-        @endif
+                @endif
+            </div>
+        </div>
     </div>
 @endsection
