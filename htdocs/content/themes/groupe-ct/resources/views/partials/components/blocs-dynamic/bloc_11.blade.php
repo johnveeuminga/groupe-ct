9e82ffbd39b74d431988c819a78bd579b4906475
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-25 19:52:48 -0400

    2.1 subpagesushushush

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_11.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_11.blade.php
@@ -2,29 +2,29 @@
 <!-- prod-logiciel MAIN CONTAINER START -->
 <div class="prod-logiciel-main-container default-padding default-width">
     @foreach($block->get_field('bloc_11_software_products') as $product)
-        <div class="prod-logiciel-container {{ $block['bloc_11_slide_image_left'] ? '' : 'reverse-elem' }}"><!-- reverse-elem : to swap img and text div -->
+        <div class="prod-logiciel-container {{ $product['bloc_11_slide_image_left'] ? '' : 'reverse-elem' }}"><!-- reverse-elem : to swap img and text div -->
             <div class="prod-logiciel-img-container">
-                @if (!empty($block['bloc_11_video_link']))
-                    <a class="youtube-lity" href="{{ $block['bloc_11_video_link'] }}" data-lity></a>
+                @if (!empty($product['bloc_11_video_link']))
+                    <a class="youtube-lity" href="{{ $product['bloc_11_video_link'] }}" data-lity></a>
                 @endif
-                <img class="img-video shadow" src="{{ $block['bloc_11_slide_image']['url'] }}" alt="{{ $block['bloc_11_slide_image']['alt'] }}">
-                @if (!empty($block['bloc_11_video_caption']))
-                    <span class="prod-logiciel-lang">{{ $block['bloc_11_video_caption'] }}</span>
+                <img class="img-video shadow" src="{{ $product['bloc_11_slide_image']['url'] }}" alt="{{ $product['bloc_11_slide_image']['alt'] }}">
+                @if (!empty($product['bloc_11_video_caption']))
+                    <span class="prod-logiciel-lang">{{ $product['bloc_11_video_caption'] }}</span>
                 @endif
             </div>
             <div class="buffer"></div>
             <div class="prod-logiciel-text-container">
-                @if (!empty($block['bloc_11_title']))
-                    <h3 class="prod-logiciel-title">{{ $block['bloc_11_title'] }}</h3>
+                @if (!empty($product['bloc_11_title']))
+                    <h3 class="prod-logiciel-title">{{ $product['bloc_11_title'] }}</h3>
                 @endif
-                @if (!empty($block['bloc_11_subtitle']))
-                    <p class="prod-logiciel-subtitle">{{ $block['bloc_11_subtitle'] }}</p>
+                @if (!empty($product['bloc_11_subtitle']))
+                    <p class="prod-logiciel-subtitle">{{ $product['bloc_11_subtitle'] }}</p>
                 @endif
-                @if (!empty($block['bloc_11_description']))
-                    <p class="prod-logiciel-text">{{ $block['bloc_11_description'] }}</p>
+                @if (!empty($product['bloc_11_description']))
+                    <p class="prod-logiciel-text">{{ $product['bloc_11_description'] }}</p>
                 @endif
-                @if ($block['bloc_11_brochure'])
-                    <a class="brochure-download-pdf-white" href="{{ $block['bloc_11_brochure']['url'] }}" target="_blank"><?php pll__('BROCHURE'); ?></a>
+                @if ($product['bloc_11_brochure'])
+                    <a class="brochure-download-pdf-white" href="{{ $product['bloc_11_brochure']['url'] }}" target="_blank"><?php pll__('BROCHURE'); ?></a>
                 @endif
             </div>
         </div>
