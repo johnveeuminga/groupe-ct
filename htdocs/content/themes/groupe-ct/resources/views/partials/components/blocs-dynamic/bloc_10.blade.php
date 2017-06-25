c88c1f6e992c3a62234e39380d25f35113b88d0e
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-25 15:11:00 -0400

    bloc 10

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_10.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_10.blade.php
@@ -16,18 +16,19 @@
                 @foreach($block->get_field('bloc_10_products') as $product)
                     <div class="product-container">
                         <div class="product-inner-container">
-                            <div class="product-img-container">
-                                <img class="product-img" src="{{ $product['bloc_10_image']['url'] }}" alt="{{ $product['bloc_10_image']['alt'] }}">
-                            </div>
-                            <div class="product-text-container">
-                                <h3 class="product-title">{{ $product['bloc_10_title'] }}</h3>
-                                <p class="product-desc">{{ $product['bloc_10_description'] }}</p>
-
-                                <div class="brochure-download-container">
-                                    @if ($product['bloc_10_product_pdf'])
-                                        <a class="brochure-download-pdf-blue" href="{{ $product['bloc_10_product_pdf']['url'] }}" target="_blank">{{ pll__('BROCHURE') }}</a>
-                                    @endif
+                            <div class="content-container">
+                                <div class="product-img-container">
+                                    <img class="product-img" src="{{ $product['bloc_10_image']['url'] }}" alt="{{ $product['bloc_10_image']['alt'] }}">
                                 </div>
+                                <div class="product-text-container">
+                                    <h3 class="product-title">{{ $product['bloc_10_title'] }}</h3>
+                                    <p class="product-desc">{{ $product['bloc_10_description'] }}</p>
+                                </div>
+                            </div>
+                            <div class="brochure-download-container">
+                                @if ($product['bloc_10_product_pdf'])
+                                    <a class="brochure-download-pdf-blue" href="{{ $product['bloc_10_product_pdf']['url'] }}" target="_blank">{{ pll__('BROCHURE') }}</a>
+                                @endif
                             </div>
                         </div>
                     </div>
