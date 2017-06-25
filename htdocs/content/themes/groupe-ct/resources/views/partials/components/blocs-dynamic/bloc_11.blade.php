f9362b007f39d64ebcd762f6824a5cb2f31151d9
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-25 19:26:41 -0400

    Added dimension

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_11.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_11.blade.php
@@ -5,16 +5,28 @@
     <!-- THE ELEMENT START  -->
     <div class="prod-logiciel-container {{ $block->get_field('bloc_11_slide_image_left') ? '' : 'reverse-elem' }}"><!-- reverse-elem : to swap img and text div -->
         <div class="prod-logiciel-img-container">
-            <a class="youtube-lity" href="{{ $block->get_field('bloc_11_video_link') }}" data-lity></a>
-            <img class="img-video shadow" src="{{ $block->get_field('bloc_11_slide_image')['url'] }}" alt="{{ $block->get_field('bloc_11_slide_image')['alt'] }}">
-            <span class="prod-logiciel-lang">{{ $block->get_field('bloc_11_video_caption') }}</span>
+            @if (!empty($block->get_field('bloc_11_video_link')))
+                <a class="youtube-lity" href="{{ $block->get_field('bloc_11_video_link') }}" data-lity></a>
+                <img class="img-video shadow" src="{{ $block->get_field('bloc_11_slide_image')['url'] }}" alt="{{ $block->get_field('bloc_11_slide_image')['alt'] }}">
+            @endif
+            @if (!empty($block->get_field('bloc_11_video_caption')))
+                <span class="prod-logiciel-lang">{{ $block->get_field('bloc_11_video_caption') }}</span>
+            @endif
         </div>
         <div class="buffer"></div>
         <div class="prod-logiciel-text-container">
-            <h3 class="prod-logiciel-title">{{ $block->get_field('bloc_11_title') }}</h3>
-            <p class="prod-logiciel-subtitle">{{ $block->get_field('bloc_11_subtitle') }}</p>
-            <p class="prod-logiciel-text">{{ $block->get_field('bloc_11_description') }}</p>
-            <a class="brochure-download-pdf-white" href="{{ $block->get_field('bloc_11_brochure')['url'] }}"><?php _e('BROCHURE', THEME_TEXTDOMAIN); ?></a>
+            @if (!empty($block->get_field('bloc_11_title')))
+                <h3 class="prod-logiciel-title">{{ $block->get_field('bloc_11_title') }}</h3>
+            @endif
+            @if (!empty($block->get_field('bloc_11_subtitle')))
+                <p class="prod-logiciel-subtitle">{{ $block->get_field('bloc_11_subtitle') }}</p>
+            @endif
+            @if (!empty($block->get_field('bloc_11_description')))
+                <p class="prod-logiciel-text">{{ $block->get_field('bloc_11_description') }}</p>
+            @endif
+            @if ($block->get_field('bloc_11_brochure'))
+                <a class="brochure-download-pdf-white" href="{{ $block->get_field('bloc_11_brochure')['url'] }}"><?php _e('BROCHURE', THEME_TEXTDOMAIN); ?></a>
+            @endif
         </div>
     </div>
     <!-- THE ELEMENT END  -->
