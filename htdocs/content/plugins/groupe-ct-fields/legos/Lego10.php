dff5b9de87b58d8d181c4e2976e05976eacdef76
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-25 14:08:41 -0400

    Rework bloc 10 backend

--- htdocs/content/plugins/groupe-ct-fields/legos/Lego10.php
+++ htdocs/content/plugins/groupe-ct-fields/legos/Lego10.php
@@ -9,10 +9,12 @@ class Lego10 extends BaseLego implements LegoInterface
     public function get_unique_fields()
     {
         return [
-            $this->generate_image('bloc_3_image',            __('Image', 'GROUPE-CT'), __('Width : 295px | Height : 255px', 'GROUPE-CT') ),
-            $this->generate_text('bloc_10_title', 'Title'),
-            $this->generate_textarea('bloc_10_description', 'Product Description'),
-            $this->generate_file('bloc_10_product_pdf', 'PDF File', 'pdf')
+            $this->generate_repeater('bloc_8_collapse',     __('Collapsible Drawer', 'GROUPE-CT'), [
+                $this->generate_image('bloc_3_image',            __('Image', 'GROUPE-CT'), __('Width : 295px | Height : 255px', 'GROUPE-CT') ),
+                $this->generate_text('bloc_10_title', 'Title'),
+                $this->generate_textarea('bloc_10_description', 'Product Description'),
+                $this->generate_file('bloc_10_product_pdf', 'PDF File', 'pdf')
+            ]),
         ];
 
         return [];
