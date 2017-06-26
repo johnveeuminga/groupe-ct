62d7f1c5bfe9cdb7ed765e511dd8bd85307e99dc
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-26 09:17:56 -0400

    template 5.0ushush

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/publication/publication-list.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/publication/publication-list.blade.php
@@ -20,7 +20,6 @@
                 @for ($i = 1; $i <= $count; $i++)
                     <a class="publication-nav-item pagination @if ($i === get_query_var( 'paged' ) || (get_query_var( 'paged' ) === 0 && $i === 1)) active-pagination @endif" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_5_0_PUBLICATIONS) }}page/{{ $i }}">{{ $i }}</a>
                 @endfor
-                <a class="publication-nav-item pagination next" href="#"><img src="{{ themosis_assets() }}/images/icon/chevron-right-grey.png" alt=""></a>
                 <a class="publication-nav-item cta-pub-right" href="#">{{ pll__('Page suivante') }}</a>
             </div>
         </div>
@@ -58,7 +57,6 @@
                 @for ($i = 1; $i <= $count; $i++)
                     <a class="publication-nav-item pagination @if ($i === get_query_var( 'paged' )) active-pagination @endif" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_5_0_PUBLICATIONS) }}page/{{ $i }}">{{ $i }}</a>
                 @endfor
-                <a class="publication-nav-item pagination next" href="#"><img src="{{ themosis_assets() }}/images/icon/chevron-right-grey.png" alt=""></a>
                 <a class="publication-nav-item cta-pub-right" href="#">{{ pll__('Page suivante') }}</a>
             </div>
         </div>
