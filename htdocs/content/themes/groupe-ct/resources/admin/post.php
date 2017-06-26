ef791393588331d487ff453346cc844eafa92478
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-26 08:40:41 -0400

    template 5.0ush

--- htdocs/content/themes/groupe-ct/resources/admin/post.php
+++ htdocs/content/themes/groupe-ct/resources/admin/post.php
@@ -10,6 +10,7 @@ acf_add_local_field_group(array (
         $acf_fields->generate_true_false('post_show_date', __('Show Date', GROUPE_CT)),
         $acf_fields->generate_true_false('post_is_featured', __('Is featured?', GROUPE_CT)),
         $acf_fields->generate_image('post_featured_image', __('Featured image', GROUPE_CT), __('Minimum Width : 585px | Minimum Height : 590px', 'GROUPE-CT')),
+        $acf_fields->generate_image('post_standard_image', __('Standard image', GROUPE_CT), __('Minimum Width : 585px | Minimum Height : 590px', 'GROUPE-CT')),
     ),
     'location' => array (
         array (
