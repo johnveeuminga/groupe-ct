f1b7ca69fcb9e1da190cd28acaa5fc9e7ae9566b
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-25 22:31:53 -0400

    5.0 category

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/publication/publication-list.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/publication/publication-list.blade.php
@@ -9,7 +9,7 @@
             <ul class="menu-item-container">
                 @foreach (get_terms('category') as $category)
                     @if ($category->name !== 'Uncategorized')
-                        <li class="menu-item"><a class="primary-btn-grey" href="#" data-target="{{ $category->term_id }}">{{ $category->name }}</a></li>
+                        <li class="menu-item"><a class="primary-btn-grey" href="#" data-target="">{{ $category->name }}</a></li>
                     @endif
                 @endforeach
             </ul>
