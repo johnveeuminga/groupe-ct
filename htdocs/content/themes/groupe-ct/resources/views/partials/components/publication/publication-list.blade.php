98e3611422861425b9369cfeb27002fc7eb9749a
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-26 10:15:49 -0400

    template 5.0ushushush

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/publication/publication-list.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/publication/publication-list.blade.php
@@ -23,7 +23,7 @@
                 @for ($i = 1; $i <= $count; $i++)
                     <a class="publication-nav-item pagination @if ($i === get_query_var( 'paged' ) || (get_query_var( 'paged' ) === 0 && $i === 1)) active-pagination @endif" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_5_0_PUBLICATIONS) }}page/{{ $i }}">{{ $i }}</a>
                 @endfor
-                @if (get_query_var( 'paged' ) === $count)
+                @if (get_query_var( 'paged' ) < $count)
                     <a class="publication-nav-item cta-pub-right" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_5_0_PUBLICATIONS) }}page/{{ get_query_var( 'paged' ) + 1 }}">{{ pll__('Page suivante') }}</a>
                 @endif
             </div>
@@ -65,9 +65,11 @@
                 @for ($i = 1; $i <= $count; $i++)
                     <a class="publication-nav-item pagination @if ($i === get_query_var( 'paged' )) active-pagination @endif" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_5_0_PUBLICATIONS) }}page/{{ $i }}">{{ $i }}</a>
                 @endfor
-                @if (get_query_var( 'paged' ) === $count)
-                    <a class="publication-nav-item cta-pub-right" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_5_0_PUBLICATIONS) }}page/{{ get_query_var( 'paged' ) + 1 }}">{{ pll__('Page suivante') }}</a>
-                @endif
+                <a class="publication-nav-item cta-pub-right" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_5_0_PUBLICATIONS) }}page/{{ get_query_var( 'paged' ) + 1 }}">
+                    @if (get_query_var( 'paged' ) === $count)
+                        {{ pll__('Page suivante') }}
+                    @endif
+                </a>
             </div>
         </div>
 
