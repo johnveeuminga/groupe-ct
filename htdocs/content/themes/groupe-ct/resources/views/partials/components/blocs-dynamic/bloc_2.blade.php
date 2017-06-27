3026ebb03f929db0043908fe6744dc3fc6ea9558
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-27 12:32:25 -0400

    Fix facture pdf

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_2.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_2.blade.php
@@ -5,31 +5,31 @@
             <div class="slide-container slide{{ $i }} @if($i == 0) {{ 'active-slide' }} @endif @if($slide['bloc_2_slide_image_left'] === false) reverse-elem @endif">
 
                 <div class="img-container">
-                    <img src="{{ $block['bloc_2_image']['url'] }}" alt="{{ $block['bloc_2_image']['alt'] }}">
+                    <img src="{{ $slide['bloc_2_image']['url'] }}" alt="{{ $slide['bloc_2_image']['alt'] }}">
                 </div>
                 <div class="buffer"></div>
                 <div class="text-container">
-                    @if (!empty($block['bloc_2_subheader']))<p class="upper-title">{{ $block['bloc_2_subheader'] }}</p>@endif
-                    @if (!empty($block['bloc_2_title']))<h2 class="text-container-title">{{ $block['bloc_2_title'] }}</h2>@endif
-                    @if (!empty($block['bloc_2_text']))<p class="text">{!! nl2br($block['bloc_2_text']) !!}</p>@endif
-                    @if (!empty($block['bloc_2_phone_1']) || !empty($block['bloc_2_phone_2']))
+                    @if (!empty($slide['bloc_2_subheader']))<p class="upper-title">{{ $slide['bloc_2_subheader'] }}</p>@endif
+                    @if (!empty($slide['bloc_2_title']))<h2 class="text-container-title">{{ $slide['bloc_2_title'] }}</h2>@endif
+                    @if (!empty($slide['bloc_2_text']))<p class="text">{!! nl2br($slide['bloc_2_text']) !!}</p>@endif
+                    @if (!empty($slide['bloc_2_phone_1']) || !empty($slide['bloc_2_phone_2']))
                         <div class="contact-container">
-                            @if (!empty($block['bloc_2_phone_1']))
+                            @if (!empty($slide['bloc_2_phone_1']))
                                 <div class="contact-content">
-                                    @if (!empty($block['bloc_2_phone_1_location']))<span class="contact-location">{{ $block['bloc_2_phone_1_location'] }}</span>@endif
-                                    @if (!empty($block['bloc_2_phone_1_label']))<a class="contact-phone" href="tel:+{{ $block['bloc_2_phone_1'] }}">{{ $block['bloc_2_phone_1_label'] }}</a>@endif
+                                    @if (!empty($slide['bloc_2_phone_1_location']))<span class="contact-location">{{ $slide['bloc_2_phone_1_location'] }}</span>@endif
+                                    @if (!empty($slide['bloc_2_phone_1_label']))<a class="contact-phone" href="tel:+{{ $slide['bloc_2_phone_1'] }}">{{ $slide['bloc_2_phone_1_label'] }}</a>@endif
                                 </div>
                             @endif
-                            @if (!empty($block['bloc_2_phone_2']))
+                            @if (!empty($slide['bloc_2_phone_2']))
                                 <div class="contact-content">
-                                    @if (!empty($block['bloc_2_phone_2_location']))<span class="contact-location">{{ $block['bloc_2_phone_2_location'] }}</span>@endif
-                                    @if (!empty($block['bloc_2_phone_2_label']))<a class="contact-phone" href="tel:+{{ $block['bloc_2_phone_2'] }}">{{ $block['bloc_2_phone_2_label'] }}</a>@endif
+                                    @if (!empty($slide['bloc_2_phone_2_location']))<span class="contact-location">{{ $slide['bloc_2_phone_2_location'] }}</span>@endif
+                                    @if (!empty($slide['bloc_2_phone_2_label']))<a class="contact-phone" href="tel:+{{ $slide['bloc_2_phone_2'] }}">{{ $slide['bloc_2_phone_2_label'] }}</a>@endif
                                 </div>
                             @endif
                         </div>
                     @endif
                     <div>
-                        @if ($block['bloc_2_cta_label'])<a class="btn-bloc2 primary-btn-blue @if ($block['bloc_2_cta_open_contact']) open-contact-form @endif" href="@if($block['bloc_2_cta_open_contact'])#@else{{ $block['bloc_2_cta_link'] }}@endif" @if($block['bloc_2_cta_blank'])target='_blank'@endif>{{ $block['bloc_2_cta_label'] }}</a>@endif
+                        @if ($slide['bloc_2_cta_label'])<a class="btn-bloc2 primary-btn-blue @if ($slide['bloc_2_cta_open_contact']) open-contact-form @endif" href="@if($slide['bloc_2_cta_open_contact'])#@else{{ $slide['bloc_2_cta_link'] }}@endif" @if($slide['bloc_2_cta_blank'])target='_blank'@endif>{{ $slide['bloc_2_cta_label'] }}</a>@endif
                     </div>
                 </div>
             </div>
