2daae4459e10936ec1b6f1d7c7e1c5ee2538c1d5
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-27 13:31:13 -0400

    Bloc 9 fix

--- htdocs/content/themes/groupe-ct/resources/admin/page.php
+++ htdocs/content/themes/groupe-ct/resources/admin/page.php
@@ -7,6 +7,10 @@ acf_add_local_field_group(array (
     'title' => 'Page Details',
     'fields' => array (
         $acf_fields->generate_text('page_html_title', __('Titre H1 HTML', GROUPE_CT)),
+        $acf_fields->generate_text('page_header_image_desktop', __('Header desktop', GROUPE_CT)),
+        $acf_fields->generate_image('page_header_image_desktop', __('Image Desktop', 'GROUPE-CT'), __('Minimum Width : 1920px | Minimum Height : 670px', 'GROUPE-CT') ),
+        $acf_fields->generate_image('page_header_image_mobile', __('Image Mobile', 'GROUPE-CT'), __('Minimum Width : 415px | Minimum Height : 735px', 'GROUPE-CT') ),
+
     ),
     'location' => array (
         array (
