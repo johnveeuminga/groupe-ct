f7f53f72e5cc4d983da8e124c08778814c39355d
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-26 20:29:52 -0400

    Fix timelineush

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_14.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_14.blade.php
@@ -1,21 +1,36 @@
-@foreach($block->get_field('bloc_14_jobs') as $job)
-    {{-- job INFO START --}}
-    <div class="job-info-container">
-        {{-- BLUE BAR WITH TITLE END --}}
-        <div class="job-info-inner-container">
-            <h3 class="job-info-title">{{ $job['bloc_14_job']->post_title }}</h3>
-            <p class="info job-info-office"><span><?php _e('Bureau rattaché', THEME_TEXTDOMAIN); ?> :</span>{{ $job['bloc_14_job']->job_offer_office  }}</p>
-            <p class="info job-info-adresse"><span><?php _e('Lieu de travail', THEME_TEXTDOMAIN); ?> :</span>{{ $job['bloc_14_job']->job_offer_location  }}</p>
-            <p class="info job-info-section"><span><?php _e('Département', THEME_TEXTDOMAIN); ?> :</span>{{ $job['bloc_14_job']->job_offer_dept  }}</p>
-            <p class="info job-info-limit"><span><?php _e('Date limite pour postuler', THEME_TEXTDOMAIN); ?> :</span>{{ date('Y-m-d', strtotime($job['bloc_14_job']->job_offer_deadline))  }}</p>
+<!-- Block 14 - Job Offer -->
+<div class="toggle-main-container default-padding default-width">
+    <div class="toggle-content-header">
+        <div class="toggle-title-container">
+            <h3 class="toggle-content-title" href="#"><?php _e('Emplois en vedette', GROUPE_CT); ?></h3>
+        </div>
+        <img class="toggle-content-carret" src="{{ themosis_assets() }}/images/icon/icon-chevron-white.png" alt="">
+        <div class="triangle-blue"></div>
+        <a class="open-toggle-content" href="#"></a>
+    </div>
 
-            <div class="btn-container">
+    <div class="toggle-content">
+        @foreach($block->get_field('bloc_14_jobs') as $job)
+        {{-- job INFO START --}}
+        <div class="job-info-container">
+            {{-- BLUE BAR WITH TITLE END --}}
+            <div class="job-info-inner-container">
+                <h3 class="job-info-title">{{ $job['bloc_14_job']->post_title }}</h3>
+                <p class="info job-info-office"><span><?php _e('Bureau rattaché', THEME_TEXTDOMAIN); ?> :</span>{{ $job['bloc_14_job']->job_offer_office  }}</p>
+                <p class="info job-info-adresse"><span><?php _e('Lieu de travail', THEME_TEXTDOMAIN); ?> :</span>{{ $job['bloc_14_job']->job_offer_location  }}</p>
+                <p class="info job-info-section"><span><?php _e('Département', THEME_TEXTDOMAIN); ?> :</span>{{ $job['bloc_14_job']->job_offer_dept  }}</p>
+                <p class="info job-info-limit"><span><?php _e('Date limite pour postuler', THEME_TEXTDOMAIN); ?> :</span>{{ date('Y-m-d', strtotime($job['bloc_14_job']->job_offer_deadline))  }}</p>
 
-                <a href="{{ get_permalink($job['bloc_14_job']->ID) }}" class="cta-see-more" data-lity><?php _e('en savoir plus', THEME_TEXTDOMAIN); ?></a>
-                <a class="btn primary-btn-blue" href="mailto:{{ Option::get('hr', 'hrEmail') }}"><?php _e('postuler', THEME_TEXTDOMAIN); ?></a>
+                <div class="btn-container">
+
+                    <a href="{{ get_permalink($job['bloc_14_job']->ID) }}" class="cta-see-more" data-lity><?php _e('en savoir plus', THEME_TEXTDOMAIN); ?></a>
+                    <a class="btn primary-btn-blue" href="mailto:{{ Option::get('hr', 'hrEmail') }}"><?php _e('postuler', THEME_TEXTDOMAIN); ?></a>
+                </div>
             </div>
         </div>
-    </div>
-    {{-- job INFO END --}}
-@endforeach
+       {{-- job INFO END --}}
+        @endforeach
+   </div>
+</div>
+
 
