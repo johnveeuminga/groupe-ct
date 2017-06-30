c7c37b0c358a7a82246a31ab83576937f469b5eb
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-30 16:02:04 -0400

    Fix 3.0

--- htdocs/content/themes/groupe-ct/resources/views/pages/page_3_0.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/pages/page_3_0.blade.php
@@ -2,31 +2,24 @@
 
 @section('main')
     <div class="container-fluid template-2-3-container tech-support-main-container" id="_main-container-page-{{ get_the_ID() }}">
-        <div class="bloc-product-search-main-container">
-            <div class="bloc-product-outer-container">
+        @if( have_rows('blocks') )
 
-                <div class="bg_white">
-                    @if( have_rows('blocks') )
+            @while ( have_rows('blocks') )
+                <?php the_row(); ?>
 
-                        @while ( have_rows('blocks') )
-                            <?php the_row(); ?>
+                <?php
+                    $row = get_row();
+                    $row_id = array_shift($row);
+                ?>
+                @while ( has_sub_field('bloc_flexible_content', $row_id) )
+                    <?php
+                        $block = new \Theme\Models\Bloc(get_row_layout());
+                    ?>
+                    @include($block->load_path(), ['block' => $block])
+                @endwhile
 
-                            <?php
-                                $row = get_row();
-                                $row_id = array_shift($row);
-                            ?>
-                            @while ( has_sub_field('bloc_flexible_content', $row_id) )
-                                <?php
-                                    $block = new \Theme\Models\Bloc(get_row_layout());
-                                ?>
-                                @include($block->load_path(), ['block' => $block])
-                            @endwhile
+            @endwhile
 
-                        @endwhile
-
-                    @endif
-                </div>
-            </div>
-        </div>
+        @endif
     </div>
 @endsection
