c8d86ce65ef7752cbb8a8a74adc8eb064f3c106f
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-06-30 17:02:42 -0400

    Bloc 9 fix

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/form-support-technique-02-3.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/form-support-technique-02-3.blade.php
@@ -13,7 +13,7 @@
             <div class="text-container col-xs-12">
 
                 <p>
-                    {!! nl2br(pll__('Demande d\'assistance - Description')) !!}&nbsp;<a href="#section-mon-ct" class="form-link">{{ pll__('Mon CT.') }}</a>
+                    {!! nl2br(pll__('Demande d\'assistance - Description')) !!}&nbsp;<a class="scroll-to" href="#" data-target="#section-mon-ct" class="form-link">{{ pll__('Mon CT.') }}</a>
                 </p>
             </div>
             <div class="content-container">
