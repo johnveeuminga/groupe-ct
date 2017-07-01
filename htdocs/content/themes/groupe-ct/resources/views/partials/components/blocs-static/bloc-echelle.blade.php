a5cb59397565b32c737c84061b9f8801bd5b94bc
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-01 19:20:08 -0400

    echelle numérique en

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-echelle.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-echelle.blade.php
@@ -41,9 +41,9 @@
         <a class="hover-zone-container" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_1_1_ENVIRONNEMENT_PAPIER) }}"><div class="hover-zone zone-papier" data-target="#hover-papier"></div></a>
         <a class="hover-zone-container" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_1_2_ENVIRONNEMENT_HYBRIDE) }}"><div class="hover-zone zone-hybride" data-target="#hover-hybride"></div></a>
         <a class="hover-zone-container" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_1_3_ENVIRONNEMENT_NUMERIQUE) }}"><div class="hover-zone zone-numerique" data-target="#hover-numerique"></div></a>
-        <img id="echelle-background-image" src="{{ themosis_assets() }}/images/echelle/desktop/main.png" class="" />
-        <img id="hover-papier" class="{{ PageHelper::get_page_id(PageHelper::PAGE_1_1_ENVIRONNEMENT_PAPIER) === get_the_ID() ? 'active' : '' }}" src="{{ themosis_assets() }}/images/echelle/desktop/hover-papier.png">
-        <img id="hover-hybride" class="{{ PageHelper::get_page_id(PageHelper::PAGE_1_2_ENVIRONNEMENT_HYBRIDE) === get_the_ID() ? 'active' : '' }}" src="{{ themosis_assets() }}/images/echelle/desktop/hover-hybride.png">
-        <img id="hover-numerique"  class="{{ PageHelper::get_page_id(PageHelper::PAGE_1_3_ENVIRONNEMENT_NUMERIQUE) === get_the_ID() ? 'active' : '' }}" src="{{ themosis_assets() }}/images/echelle/desktop/hover-numerique.png">
+        <img id="echelle-background-image" src="{{ themosis_assets() }}/images/echelle/desktop/main-{{ pll_current_language() }}.png" class="" />
+        <img id="hover-papier" class="{{ PageHelper::get_page_id(PageHelper::PAGE_1_1_ENVIRONNEMENT_PAPIER) === get_the_ID() ? 'active' : '' }}" src="{{ themosis_assets() }}/images/echelle/desktop/hover-papier-{{ pll_current_language() }}.png">
+        <img id="hover-hybride" class="{{ PageHelper::get_page_id(PageHelper::PAGE_1_2_ENVIRONNEMENT_HYBRIDE) === get_the_ID() ? 'active' : '' }}" src="{{ themosis_assets() }}/images/echelle/desktop/hover-hybride-{{ pll_current_language() }}.png">
+        <img id="hover-numerique"  class="{{ PageHelper::get_page_id(PageHelper::PAGE_1_3_ENVIRONNEMENT_NUMERIQUE) === get_the_ID() ? 'active' : '' }}" src="{{ themosis_assets() }}/images/echelle/desktop/hover-numerique-{{ pll_current_language() }}.png">
     </div>
 </section>
