06e9195b8769d128b3eede19e4828aa02b3a0166
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-01 13:14:17 -0400

    home field

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero.blade.php
@@ -2,27 +2,29 @@
 
     @include('partials.components.header.sub-menu')
     @include('partials.components.header.contact-form')
-    <div class="hero-content-container">
 
-        <div class="hero-content">
-            <div class="hero-title-container">
-                <h1 class="hero-title">NUMÉRISER VOTRE </br> <span class="red-border">FLUX DE TRAVAIL</span></h1>
-            </div>
-            <div class="hero-subtitle-container">
-                <p class="hero-subtitle"><span>Groupe CT</span> aide les entreprises à optimiser leur environnement papier vers une gestion électronique des documents.</p>
-                <a class="hero-btn primary-btn" href="#">LOREM IPSUM DOLOR</a>
-            </div>
-            <a href="#"><img class="hero-arrow" src="{{ themosis_assets() }}/images/icon/icon-hero-arrow.png" alt=""></a>
+    <div class="img-plus-text default-padding default-width">
+        <div class="slider-main-container">
+            @foreach(get_field('home_slider') as $i => $slide)
+                <div class="slide-container slide{{ $i }} @if($i == 0) {{ 'active-slide' }} @endif">
+                    <div class="img-container">
+                        <img src="{{ $slide['bloc_1_slide_image_desktop']['url'] }}" alt="{{ $slide['bloc_1_slide_image_desktop']['alt'] }}">
+                    </div>
+                    <div class="buffer"></div>
+
+                    <div class="text-container">
+                        <h1 class="text-container-title">{{ $slide['bloc_1_slide_title'] }}</h1>
+                        <h2 class="text-container-subtitle">his subtitle shouldn't exists?!</h2>
+                        {{--<a class="btn-bloc2 primary-btn-blue" href="{{ $slide['bloc_1_slide_cta_link'] }}">{{ $slide['bloc_1_slide_cta_label'] }}</a>--}}
+                    </div>
+                </div>
+            @endforeach
         </div>
-
+        <div class="slider-link-container-bloc02">
+            @foreach($block->get_field('bloc_1_slider') as $slide)
+                <a class="slider-dot slide{{ $i }} blue-dot @if($i == 0) {{ 'active-dot-blue' }} @endif" href="#"></a>
+            @endforeach
+        </div>
     </div>
-
-    <div class="triangle"></div>
-
-    <div class="hero-contact-us-container">
-
-    </div>
-
-
 
 </section>
