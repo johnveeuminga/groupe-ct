08991e679be954b7dd055898f4628d04b6ac4acd
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-01 12:15:34 -0400

    home field

--- htdocs/content/themes/groupe-ct/resources/admin/page.php
+++ htdocs/content/themes/groupe-ct/resources/admin/page.php
@@ -1,5 +1,4 @@
 <?php
-
 $acf_fields = new BaseLego();
 
 acf_add_local_field_group(array (
@@ -19,6 +18,16 @@ acf_add_local_field_group(array (
                 'operator' => '==',
                 'value' => 'page',
             ),
+            array (
+                'param' => 'page',
+                'operator' => '!=',
+                'value' => '158',
+            ),
+            array (
+                'param' => 'page',
+                'operator' => '!=',
+                'value' => '255',
+            ),
         ),
     ),
     'menu_order' => 0,
@@ -30,3 +39,45 @@ acf_add_local_field_group(array (
     'active' => 1,
     'description' => '',
 ));
+
+acf_add_local_field_group(array (
+    'key' => 'page_field_group',
+    'title' => 'Page Details',
+    'fields' => array (
+        $acf_fields->generate_repeater('bloc_1_slider', __('Slider', 'GROUPE-CT'),
+            [
+                $acf_fields->generate_image('bloc_1_slide_image_desktop', __('Image', 'GROUPE-CT'), __('Minimum Width : 1920px | Minimum Height : 1175px', 'GROUPE-CT') ),
+                $acf_fields->generate_image('bloc_1_slide_image_mobile', __('Image', 'GROUPE-CT'), __('Minimum Width : 1920px | Minimum Height : 1175px', 'GROUPE-CT') ),
+                $acf_fields->generate_text('bloc_1_slide_title', __('Title', 'GROUPE-CT') ),
+                $acf_fields->generate_cta('bloc_1_slide')[0],
+                $acf_fields->generate_cta('bloc_1_slide')[1],
+                $acf_fields->generate_cta('bloc_1_slide')[2],
+            ],
+            1,
+            6
+        ),
+    ),
+    'location' => array (
+        array (
+            array (
+                'param' => 'page',
+                'operator' => '==',
+                'value' => '158',
+            ),
+            array (
+                'param' => 'page',
+                'operator' => '==',
+                'value' => '255',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+
