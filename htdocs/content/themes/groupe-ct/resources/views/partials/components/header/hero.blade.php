8a863dda65c82abfae92a16d8181b9a97990b8d6
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-01 13:21:23 -0400

    home fieldush

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero.blade.php
@@ -3,9 +3,10 @@
     @include('partials.components.header.sub-menu')
     @include('partials.components.header.contact-form')
 
-    <div class="img-plus-text default-padding default-width">
-        <div class="slider-main-container">
-            @foreach(get_field('home_slider') as $i => $slide)
+
+    <div class="hero-content-container">
+        @foreach(get_field('home_slider') as $i => $slide)
+            <div class="hero-content" style="background-image: url()">
                 <div class="slide-container slide{{ $i }} @if($i == 0) {{ 'active-slide' }} @endif">
                     <div class="img-container">
                         <img src="{{ $slide['bloc_1_slide_image_desktop']['url'] }}" alt="{{ $slide['bloc_1_slide_image_desktop']['alt'] }}">
@@ -14,17 +15,19 @@
 
                     <div class="text-container">
                         <h1 class="text-container-title">{{ $slide['bloc_1_slide_title'] }}</h1>
-                        <h2 class="text-container-subtitle">his subtitle shouldn't exists?!</h2>
                         {{--<a class="btn-bloc2 primary-btn-blue" href="{{ $slide['bloc_1_slide_cta_link'] }}">{{ $slide['bloc_1_slide_cta_label'] }}</a>--}}
                     </div>
                 </div>
-            @endforeach
-        </div>
-        <div class="slider-link-container-bloc02">
+
+                <a href="#"><img class="hero-arrow" src="{{ themosis_assets() }}/images/icon/icon-hero-arrow.png" alt=""></a>
+            </div>
+        @endforeach
+        <div class="slider-link-container-bloc01">
             @foreach(get_field('home_slider') as $i => $slide)
                 <a class="slider-dot slide{{ $i }} blue-dot @if($i == 0) {{ 'active-dot-blue' }} @endif" href="#"></a>
             @endforeach
         </div>
-    </div>
 
+        <div class="triangle"></div>
+    </div>
 </section>
