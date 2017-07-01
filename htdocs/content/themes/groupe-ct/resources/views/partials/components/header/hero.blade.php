4713022af4786c84b5a0cc02a8e0f0e85c79f0c6
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-01 15:10:12 -0400

    home fieldush

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero.blade.php
@@ -6,20 +6,18 @@
 
     <div class="hero-content-container">
         @foreach(get_field('home_slider') as $i => $slide)
-            <div class="hero-content" style="background-image: url('')">
-                <div class="slide-container slide{{ $i }} @if($i == 0) {{ 'active-slide' }} @endif">
-                    <div class="img-container">
-                        <img src="{{ $slide['bloc_1_slide_image_desktop']['url'] }}" alt="{{ $slide['bloc_1_slide_image_desktop']['alt'] }}">
-                        <div class="overlay"></div>
-                        <div class="triangle"></div>
-                    </div>
-                    <div class="buffer"></div>
+            <div class="hero-content slide-container slide{{ $i }} @if($i == 0) {{ 'active-slide' }} @endif">
+                <div class="img-container">
+                    <img src="{{ $slide['bloc_1_slide_image_desktop']['url'] }}" alt="{{ $slide['bloc_1_slide_image_desktop']['alt'] }}">
+                    <div class="overlay"></div>
+                    <div class="triangle"></div>
+                </div>
+                <div class="buffer"></div>
 
-                    <div class="text-container">
-                        <h1 class="hero-title">{!! $slide['bloc_1_slide_title'] !!}</h1>
-                        <h2 class="hero-subtitle">{!! $slide['bloc_1_slide_description'] !!}</h2>
-                        {{--<a class="btn-bloc2 primary-btn-blue" href="{{ $slide['bloc_1_slide_cta_link'] }}">{{ $slide['bloc_1_slide_cta_label'] }}</a>--}}
-                    </div>
+                <div class="text-container">
+                    <h1 class="hero-title">{!! $slide['bloc_1_slide_title'] !!}</h1>
+                    <h2 class="hero-subtitle">{!! $slide['bloc_1_slide_description'] !!}</h2>
+                    {{--<a class="btn-bloc2 primary-btn-blue" href="{{ $slide['bloc_1_slide_cta_link'] }}">{{ $slide['bloc_1_slide_cta_label'] }}</a>--}}
                 </div>
 
                 <a href="#"><img class="hero-arrow" src="{{ themosis_assets() }}/images/icon/icon-hero-arrow.png" alt=""></a>
@@ -31,6 +29,5 @@
             @endforeach
         </div>
 
-        <div class="triangle"></div>
     </div>
 </section>
