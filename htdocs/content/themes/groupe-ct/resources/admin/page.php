5ce05ae7fa4c261e365e135c603121605c142bf6
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-01 12:17:16 -0400

    home field

--- htdocs/content/themes/groupe-ct/resources/admin/page.php
+++ htdocs/content/themes/groupe-ct/resources/admin/page.php
@@ -64,6 +64,8 @@ acf_add_local_field_group(array (
                 'operator' => '==',
                 'value' => '158',
             ),
+        ),
+        array (
             array (
                 'param' => 'page',
                 'operator' => '==',
