e04462c2efa209f3fdd9d35e6eaa9621d0fdcb9b
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-01 20:46:33 -0400

    Bloc 9 fix

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_14.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_14.blade.php
@@ -16,7 +16,7 @@
             {{-- BLUE BAR WITH TITLE END --}}
             <div class="job-info-inner-container">
                 <h3 class="job-info-title">{{ $job['bloc_14_job']->post_title }}</h3>
-                <p class="info job-info-office"><span><?php _e('Bureau rattaché', THEME_TEXTDOMAIN); ?> :</span>{{ $job['bloc_14_job']->job_offer_office  }}</p>
+{{--                <p class="info job-info-office"><span><?php _e('Bureau rattaché', THEME_TEXTDOMAIN); ?> :</span>{{ $job['bloc_14_job']->job_offer_office  }}</p>--}}
                 <p class="info job-info-adresse"><span><?php _e('Lieu de travail', THEME_TEXTDOMAIN); ?> :</span>{{ $job['bloc_14_job']->job_offer_location  }}</p>
                 <p class="info job-info-section"><span><?php _e('Département', THEME_TEXTDOMAIN); ?> :</span>{{ $job['bloc_14_job']->job_offer_dept  }}</p>
                 <p class="info job-info-limit"><span><?php _e('Date limite pour postuler', THEME_TEXTDOMAIN); ?> :</span>{{ date('Y-m-d', strtotime($job['bloc_14_job']->job_offer_deadline))  }}</p>
