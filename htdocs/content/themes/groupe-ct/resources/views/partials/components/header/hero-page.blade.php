ff05cb0a75a7308ab8af8da9e07237cf0f66fbad
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-02 15:44:48 -0400

    fix css

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero-page.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero-page.blade.php
@@ -1,3 +1,22 @@
+<style scoped>
+    .hero-content-container {
+        background: black;
+    }
+
+    @if (is_array(get_field('page_header_image_desktop')))
+        .hero-content-container {
+            background-image: url({{ get_field('page_header_image_desktop')['url'] }});
+        }
+    @endif
+    @if (is_array(get_field('page_header_image_mobile')))
+        @media (max-width: 768px) { /* or 301 if you want really the same as previously.  */
+            .hero-content-container {
+                background-image: url({{ get_field('page_header_image_mobile')['url'] }});
+            }
+        }
+    @endif
+</style>
+
 <section class="hero-page row">
 
     @include('partials.components.header.sub-menu')
@@ -9,10 +28,10 @@
                 <h1 class="hero-title">@if(get_field('page_html_title', Loop::id()) && !empty(get_field('page_html_title', Loop::id()))){!! get_field('page_html_title', Loop::id())  !!} @else<span class="red-border">{{ Loop::title() }}</span>@endif</h1>
             @endloop
             @if (is_array(get_field('page_header_image_desktop')))
-                <img src="{{ get_field('page_header_image_desktop')['url'] }}" class="image-desktop" alt="{{ get_field('page_header_image_desktop')['alt'] }}">
+                {{--<img src="{{ get_field('page_header_image_desktop')['url'] }}" class="image-desktop" alt="{{ get_field('page_header_image_desktop')['alt'] }}">--}}
             @endif
             @if (is_array(get_field('page_header_image_mobile')))
-                <img src="{{ get_field('page_header_image_mobile')['url'] }}" class="image-mobile" alt="{{ get_field('page_header_image_mobile')['alt'] }}">
+                {{--<img src="{{ get_field('page_header_image_mobile')['url'] }}" class="image-mobile" alt="{{ get_field('page_header_image_mobile')['alt'] }}">--}}
             @endif
             <div class="overlay"></div>
         </div>
