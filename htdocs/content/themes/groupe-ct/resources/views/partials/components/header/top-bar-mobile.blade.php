5331efe772782fa3c00d77ea93a557b91306e50e
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-02 16:29:09 -0400

    fix css

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/header/top-bar-mobile.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/header/top-bar-mobile.blade.php
@@ -2,7 +2,7 @@
 
     <div class="top-bar-content col-xs-12 default-padding">
 
-        <div class="top-bar-logo-container"> </div>
+        <a href="{{ home_url() }}"><div class="top-bar-logo-container"> </div></a>
         <div class="top-bar-menu-btn-container">
             <a href="#menu-mobile" id="menu-btn">
                 <span></span>
