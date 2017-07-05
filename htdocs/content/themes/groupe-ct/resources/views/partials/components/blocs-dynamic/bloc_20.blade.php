52150b9311072cf04683c23b1f42a90f0c15b3b4
Rémi Clermont <remi.clermont@ctrlweb.ca>
2017-07-05 14:29:11 -0400

    Register missing translationàà

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_20.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_20.blade.php
@@ -1,5 +1,5 @@
 <section class="publication-single-main-container default-width" style="padding-top: 0;">
-    <div class="publication-container">
+    <div class="publication-container" style="padding-top: 0;">
         <div class="publication-content-container">
             <div class="wysiwyg-container">
                 {!! $block->get_field('bloc_20_wysiwyg') !!}
