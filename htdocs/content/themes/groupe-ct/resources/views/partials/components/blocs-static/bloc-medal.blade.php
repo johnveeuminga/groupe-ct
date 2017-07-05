636884116f67c308a748bf5cd3fa4745b3761008
Rémi Clermont <remi.clermont@ctrlweb.ca>
2017-07-04 22:33:52 -0400

    Register missing translation

---
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-medal.blade.php
@@ -0,0 +1,18 @@
+<div class="prod-logiciel-main-container default-padding default-width">
+    <div class="prod-logiciel-container"><!-- reverse-elem : to swap img and text div -->
+        <div class="prod-logiciel-img-container">
+            <a class="youtube-lity" href='<iframe width="560" height="315" src="https://www.youtube.com/embed/uo2dRacms_c" frameborder="0" allowfullscreen></iframe>' data-lity></a>
+
+            <img class="img-video shadow" src="{{ themosis_assets() }}/images/img/img-medaille-bg-video.jpg">
+        </div>
+        <div class="buffer"></div>
+        <div class="prod-logiciel-text-container">
+            <div class="medaille-container">
+                <h2 class="prod-logiciel-title">{{ pll__('LES MÉDAILLÉS DE LA RELÈVE') }}</h2>
+            </div>
+            <p class="prod-logiciel-text">{!! pll__('medaille description') !!}</p>
+
+        </div>
+    </div>
+</div><!-- prod-logiciel MAIN CONTAINER END -->
+
