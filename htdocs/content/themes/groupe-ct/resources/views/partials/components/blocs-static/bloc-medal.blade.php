503daa7ba841020a26589322d4f3d8ebf5a32b2e
Rémi Clermont <remi.clermont@ctrlweb.ca>
2017-07-04 22:41:00 -0400

    Register missing translation

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-medal.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-medal.blade.php
@@ -1,9 +1,8 @@
 <div class="prod-logiciel-main-container bloc-medal default-padding default-width">
     <div class="prod-logiciel-container"><!-- reverse-elem : to swap img and text div -->
         <div class="prod-logiciel-img-container">
-            <a class="youtube-lity" href='<iframe width="560" height="315" src="https://www.youtube.com/embed/uo2dRacms_c" frameborder="0" allowfullscreen></iframe>' data-lity></a>
-
             <img class="img-video shadow" src="{{ themosis_assets() }}/images/img/img-medaille-bg-video.jpg">
+            <a class="youtube-lity" href='<iframe width="560" height="315" src="https://www.youtube.com/embed/uo2dRacms_c" frameborder="0" allowfullscreen></iframe>' data-lity></a>
         </div>
         <div class="buffer"></div>
         <div class="prod-logiciel-text-container">
