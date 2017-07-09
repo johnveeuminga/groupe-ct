26f80b3937625ed86730c2b9e2c08d99217f7a24
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-09 14:32:16 -0400

    CSS Fix

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-medal.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-static/bloc-medal.blade.php
@@ -2,7 +2,7 @@
     <div class="prod-logiciel-container"><!-- reverse-elem : to swap img and text div -->
         <div class="prod-logiciel-img-container">
             <a class="youtube-lity" href='<iframe width="560" height="315" src="https://www.youtube.com/embed/uo2dRacms_c" frameborder="0" allowfullscreen></iframe>' data-lity></a>
-            <img class="img-video shadow" src="{{ themosis_assets() }}/images/img/img-medaille-bg-video.jpg">
+            <img class="img-video shadow" src="{{ themosis_assets() }}/images/img/img-medaille-bg-video-{{ pll_current_language() }}.jpg">
         </div>
         <div class="buffer"></div>
         <div class="prod-logiciel-text-container">
