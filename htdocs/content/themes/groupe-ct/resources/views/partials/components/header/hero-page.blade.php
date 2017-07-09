b4cccbafb1e9c3aae84bcfee5cbf837bf6c692d9
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-09 15:46:01 -0400

    CSS Fix

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero-page.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/header/hero-page.blade.php
@@ -17,14 +17,14 @@
     @endif
 
     @if (get_post_type( get_the_ID() === 'post'))
-        /*.hero-content-container {*/
-            {{--background-image: url({{ get_field('page_header_image_desktop', PageHelper::get_page_id(PageHelper::PAGE_5_0_PUBLICATIONS))['url'] }});--}}
-        /*}*/
-        /*@media (max-width: 767px) { !* or 301 if you want really the same as previously.  *!*/
-            /*.hero-content-container {*/
-                {{--background-image: url({{ get_field('page_header_image_mobile', PageHelper::get_page_id(PageHelper::PAGE_5_0_PUBLICATIONS))['url'] }});--}}
-            /*}*/
-        /*}*/
+        .hero-content-container {
+            background-image: url({{ get_field('page_header_image_desktop', PageHelper::get_page_id(PageHelper::PAGE_5_0_PUBLICATIONS))['url'] }});
+        }
+        @media (max-width: 767px) { /* or 301 if you want really the same as previously.  */
+            .hero-content-container {
+                background-image: url({{ get_field('page_header_image_mobile', PageHelper::get_page_id(PageHelper::PAGE_5_0_PUBLICATIONS))['url'] }});
+            }
+        }
     @endif
 </style>
 
@@ -36,13 +36,13 @@
     <div class="hero-content-container">
         <div class="hero-title-container">
 
-            {{--@if (get_post_type( get_the_ID() === 'post'))--}}
-{{--                <h1 class="hero-title">@if(get_field('page_html_title', PageHelper::get_page_id(PageHelper::PAGE_5_0_PUBLICATIONS)) && !empty(get_field('page_html_title', Loop::id()))){!! get_field('page_html_title', PageHelper::get_page_id(PageHelper::PAGE_5_0_PUBLICATIONS))  !!} @else<span class="red-border">{{ get_the_title(PageHelper::get_page_id(PageHelper::PAGE_5_0_PUBLICATIONS)) }}</span>@endif</h1>--}}
-            {{--@else--}}
+            @if (get_post_type( get_the_ID() === 'post'))
+                <h1 class="hero-title">@if(get_field('page_html_title', PageHelper::get_page_id(PageHelper::PAGE_5_0_PUBLICATIONS)) && !empty(get_field('page_html_title', Loop::id()))){!! get_field('page_html_title', PageHelper::get_page_id(PageHelper::PAGE_5_0_PUBLICATIONS))  !!} @else<span class="red-border">{{ get_the_title(PageHelper::get_page_id(PageHelper::PAGE_5_0_PUBLICATIONS)) }}</span>@endif</h1>
+            @else
                 @loop
                     <h1 class="hero-title">@if(get_field('page_html_title', Loop::id()) && !empty(get_field('page_html_title', Loop::id()))){!! get_field('page_html_title', Loop::id())  !!} @else<span class="red-border">{{ Loop::title() }}</span>@endif</h1>
                 @endloop
-            {{--@endif--}}
+            @endif
         </div>
         <div class="overlay"></div>
     </div>
