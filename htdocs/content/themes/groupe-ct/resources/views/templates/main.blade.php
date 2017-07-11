4cc311ca496e6708543749865915ae307c710372
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-10 23:11:33 -0400

    CSS fix

--- htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
@@ -45,7 +45,6 @@
 	<!-- End Google Tag Manager (noscript) -->
 	<a href="#interstitiel" data-lity>Inline</a>
 	<div id="interstitiel" class="lity-hide" style="background-image: url('{{ themosis_assets() }}/images/bg/interstiel.jpg');">
-		some content;
 		<div class="btn-container">
 			<a class="btn-bloc2 primary-btn-blue" href="#">Poursuivre votre<br/>visite</a>
 			<a class="btn-bloc2 primary-btn-blue" style="margin: 0 15px;" href="#">Lire le communiqué<br/>de presse</a>
