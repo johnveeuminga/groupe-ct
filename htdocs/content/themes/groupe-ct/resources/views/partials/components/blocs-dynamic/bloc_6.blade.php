a95952cd98f908db553da0f41768df0b651fe9c8
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-11 14:39:40 -0400

    qa performaush

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_6.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_6.blade.php
@@ -4,14 +4,19 @@
         <div class="three-col-icons-item col-md-4">
             @if (is_array($block->get_field('bloc_6_1_icon')))
                 <div class="three-col-icons-img-container">
+                    @if (!empty($block->get_field('bloc_6_2_cta_link')))
+                        <a class="cta-see-more" href="{{ $block->get_field('bloc_6_2_cta_link') }}" {{ $block->get_field('bloc_6_2_cta_blank') ? 'target="blank"' : '' }}>
+                    @endif
                     <img class="three-col-icons-img" src="{{ $block->get_field('bloc_6_1_icon')['url'] }}" alt="{{ $block->get_field('bloc_6_1_icon')['alt'] }}" >
+                    @if (!empty($block->get_field('bloc_6_2_cta_link')))
+                        </a>
+                    @endif
                 </div>
             @endif
             <div class="three-col-icon-text">
                 @if (!empty($block->get_field('bloc_6_1_title')))
                     <h3 class="three-col-icons-title">{!! $block->get_field('bloc_6_1_title') !!} </h3>
                 @endif
-
                 @if (!empty($block->get_field('bloc_6_1_description')))
                     <p class="three-col-icons-desc">{{ $block->get_field('bloc_6_1_description') }}</p>
                 @endif
@@ -24,14 +29,19 @@
         <div class="three-col-icons-item col-md-4">
             @if (is_array($block->get_field('bloc_6_2_icon')))
                 <div class="three-col-icons-img-container">
+                    @if (!empty($block->get_field('bloc_6_2_cta_link')))
+                        <a class="cta-see-more" href="{{ $block->get_field('bloc_6_2_cta_link') }}" {{ $block->get_field('bloc_6_2_cta_blank') ? 'target="blank"' : '' }}>
+                    @endif
                     <img class="three-col-icons-img" src="{{ $block->get_field('bloc_6_2_icon')['url'] }}" alt="{{ $block->get_field('bloc_6_2_icon')['alt'] }}" >
+                    @if (!empty($block->get_field('bloc_6_2_cta_link')))
+                        </a>
+                    @endif
                 </div>
             @endif
             <div class="three-col-icon-text">
                 @if (!empty($block->get_field('bloc_6_2_title')))
                     <h3 class="three-col-icons-title">{!! $block->get_field('bloc_6_2_title') !!} </h3>
                 @endif
-
                 @if (!empty($block->get_field('bloc_6_2_description')))
                     <p class="three-col-icons-desc">{{ $block->get_field('bloc_6_2_description') }}</p>
                 @endif
@@ -45,7 +55,13 @@
         <div class="three-col-icons-item col-md-4">
             @if (is_array($block->get_field('bloc_6_3_icon')))
                 <div class="three-col-icons-img-container">
+                    @if (!empty($block->get_field('bloc_6_2_cta_link')))
+                        <a class="cta-see-more" href="{{ $block->get_field('bloc_6_2_cta_link') }}" {{ $block->get_field('bloc_6_2_cta_blank') ? 'target="blank"' : '' }}>
+                    @endif
                     <img class="three-col-icons-img" src="{{ $block->get_field('bloc_6_3_icon')['url'] }}" alt="{{ $block->get_field('bloc_6_3_icon')['alt'] }}" >
+                    @if (!empty($block->get_field('bloc_6_2_cta_link')))
+                        </a>
+                    @endif
                 </div>
             @endif
             <div class="three-col-icon-text">
