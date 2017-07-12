e72b375c684df656ab0e4f0153e2d099756f4b4b
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-12 19:47:23 -0400

    qa performaushushushushushush

--- htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
@@ -60,11 +60,23 @@
 		</style>
 
 		<div id="interstitiel" class="lity-hide">
-			<div>
-				<a href="#" class="abc">abcdefg</a>
-				<div>a</div>
-				<div>b</div>
-				<div>c</div>
+			<div class="content">
+				<h2>{!! get_field('acquisition_title', $acquisition->ID) !!}</h2>
+				<p>{!! get_field('acquisition_description', $acquisition->ID) !!}</p>
+				<div class="btn-container">
+					<a class="btn-bloc2 primary-btn close-data-lity" href="#">{!! pll__('Poursuivre votre visite') !!}</a>
+					@if (!empty(get_field('acquisition_2_cta_link', $acquisition->ID)))
+						<a class="btn-bloc2 primary-btn" style="margin: 0 15px;" href="{{ get_field('acquisition_1_cta_link', $acquisition->ID) }}" @if(get_field('acquisition_1_cta_blank', $acquisition->ID)) target="_blank" @endif>{!! get_field('acquisition_1_cta_label', $acquisition->ID) !!}</a>
+					@endif
+					@if (!empty(get_field('acquisition_2_cta_link', $acquisition->ID)))
+						<a class="btn-bloc2 primary-btn" href="{{ get_field('acquisition_2_cta_link', $acquisition->ID) }}" @if(get_field('acquisition_2_cta_blank', $acquisition->ID)) target="_blank" @endif>{!! get_field('acquisition_2_cta_label', $acquisition->ID) !!}</a>
+					@endif
+				</div>
+
+				<div class="interstitiel-toggle">
+					<a href="#abc">English</a>
+					<a class="lity-close" type="button" aria-label="Close (Press escape to close)" data-lity-close="">×</a>
+				</div>
 			</div>
 		</div>
 	@endif
