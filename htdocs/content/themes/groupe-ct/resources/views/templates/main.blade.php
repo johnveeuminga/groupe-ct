fc36efb172293293fac0a0991b65ca967cd0851e
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-12 19:13:07 -0400

    qa performaushushushushushush

--- htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
@@ -76,7 +76,6 @@
 
 			<div class="interstitiel-toggle">
 				<a href="#abc" class="test">English</a>
-				<button class="lity-close" type="button" aria-label="Close (Press escape to close)" data-lity-close="">×</button>
 			</div>
 		</div>
 	@endif
