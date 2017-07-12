33875634024a64a546835667a01db26cce5e3944
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-12 19:44:41 -0400

    qa performaushushushushushush

--- htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
@@ -60,12 +60,10 @@
 		</style>
 
 		<div id="interstitiel" class="lity-hide">
-			<div class="togle">
-				<a href="#abc">test</a>
-				<a href="#abc" class="lity-close">×</a>
-				<a href="#abc" class="lity-close">×</a>
-				<a href="#abc" class="lity-close">×</a>
-			</div>
+			<a href="#abc">test</a>
+			<a href="#abc" class="lity-close">×</a>
+			<a href="#abc" class="lity-close">×</a>
+			<a href="#abc" class="lity-close">×</a>
 		</div>
 	@endif
 
