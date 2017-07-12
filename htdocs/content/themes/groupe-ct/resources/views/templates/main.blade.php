3995e854067da0a9ac9fae63958925d2a2423f25
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-12 14:40:19 -0400

    qa performaushushushushushush

--- htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
@@ -54,7 +54,7 @@
 			<div class="btn-container">
 				<a class="btn-bloc2 primary-btn close-data-lity" href="#">{!! pll__('Poursuivre votre<br/>visite') !!}</a>
 				@if (!empty(get_field('acquisition_2_cta_link', $acquisition->ID)))
-					<a class="btn-bloc2 primary-btn" style="margin: 0 15px;" href="{{ get_field('acquisition_1_cta_link', $acquisition->ID) }}">{!! get_field('acquisition_1_cta_label', $acquisition->ID) !!}</a>
+					<a class="btn-bloc2 primary-btn" style="margin: 0 15px;" href="{{ get_field('acquisition_1_cta_link', $acquisition->ID) }}" @if(get_field('acquisition_2_cta_blank', $acquisition->ID))target="_blank" @endif>{!! get_field('acquisition_1_cta_label', $acquisition->ID) !!}</a>
 				@endif
 				@if (!empty(get_field('acquisition_2_cta_link', $acquisition->ID)))
 					<a class="btn-bloc2 primary-btn" href="{{ get_field('acquisition_2_cta_link', $acquisition->ID) }}" @if(get_field('acquisition_2_cta_blank', $acquisition->ID))target="_blank" @endif>{!! get_field('acquisition_2_cta_label', $acquisition->ID) !!}</a>
