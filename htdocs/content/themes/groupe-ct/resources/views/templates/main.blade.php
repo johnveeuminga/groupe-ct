6bb149faa0aa519feef7823fb656d5fe0ef8f1cc
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-12 19:39:29 -0400

    qa performaushushushushushush

--- htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/templates/main.blade.php
@@ -72,11 +72,11 @@
 						<a class="btn-bloc2 primary-btn" href="{{ get_field('acquisition_2_cta_link', $acquisition->ID) }}" @if(get_field('acquisition_2_cta_blank', $acquisition->ID)) target="_blank" @endif>{!! get_field('acquisition_2_cta_label', $acquisition->ID) !!}</a>
 					@endif
 				</div>
-			</div>
 
-			<div class="interstitiel-toggle">
-				<a href="#abc" class="test">English</a>
-				<button class="lity-close" type="button" aria-label="Close (Press escape to close)" data-lity-close="">×</button>
+				<div class="interstitiel-toggle">
+					<a href="#abc" class="test">English</a>
+					<button class="lity-close" type="button" aria-label="Close (Press escape to close)" data-lity-close="">×</button>
+				</div>
 			</div>
 		</div>
 	@endif
