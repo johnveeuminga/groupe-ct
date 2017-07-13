b789abe28aeb30a79db11fd1b844642190c4f44f
Michael Villeneuve <michael.villeneuve@ctrlweb.ca>
2017-07-13 14:31:49 -0400

    qa performa

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/footer/footer.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/footer/footer.blade.php
@@ -80,7 +80,7 @@
 
             </div>
             <div class="legal-container">
-                <p><?php echo date("Y"); ?> &copy;{{ pll__('Groupe C.T Tous droits réservé') }}&nbsp;-&nbsp;</p><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_5_0_LEGAL) }}">{{ PageHelper::get_page_title(PageHelper::PAGE_5_0_LEGAL) }}</a>
+                <p><?php echo date("Y"); ?> &copy; {{ pll__('Groupe C.T Tous droits réservé') }}&nbsp;-&nbsp;</p><a class="nav-link" href="{{ PageHelper::get_page_permalink(PageHelper::PAGE_5_0_LEGAL) }}">{{ PageHelper::get_page_title(PageHelper::PAGE_5_0_LEGAL) }}</a>
             </div>
 
 
