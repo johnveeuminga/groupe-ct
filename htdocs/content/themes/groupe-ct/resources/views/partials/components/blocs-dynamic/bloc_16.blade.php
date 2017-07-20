e1b78b99a6e4232b54faf61e3a7e98b0f08418a1
Rémi Clermont <remi.clermont@ctrlweb.ca>
2017-07-20 16:19:44 -0400

    svgggg

--- htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_16.blade.php
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/blocs-dynamic/bloc_16.blade.php
@@ -10,7 +10,7 @@
             @foreach($block->get_field('block_16_timeline_events') as $event)
             <div class="timeline-event-container @if ($reverse) left @endif">
                 <div class="timeline-dot-container">
-                    <img class="timeline-dot dot-right" src="{{ themosis_assets() }}/images/timeline-icon/dot.png" alt="">
+                    <img class="timeline-dot dot-right" src="{{ themosis_assets() }}/images/timeline-icon/dot.svg" alt="">
                 </div>
                 <div class="event">
                     <div class="event-line"></div>
