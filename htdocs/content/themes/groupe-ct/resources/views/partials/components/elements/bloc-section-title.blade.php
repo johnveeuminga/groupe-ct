118763ee0d6b37eeace7ee0e8833808314b7cef6
John Vee Uminga <johnveeuminga@gmail.com>
2018-06-12 13:40:00 +0800

    TEst

---
+++ htdocs/content/themes/groupe-ct/resources/views/partials/components/elements/bloc-section-title.blade.php
@@ -0,0 +1,19 @@
+<div class="bloc-name row">
+    <h1>BLOC SECTION TITLE</h1>
+</div>
+
+<div class="default-padding default-width">
+
+    <!-- TITLE START -->
+    <div class="section-title-container">
+        <li class="upper-title">Amet dolor simaet</li>
+        <h1 class="title">le meilleur de l'insdustrie</h1>
+        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua
+        </p>
+    </div>
+    <!-- TITLE END -->
+</div>
+
+
+
